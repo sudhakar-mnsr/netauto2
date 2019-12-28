07e22277d6b0c471333770b44a819e96ae11e3cf
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2019-12-29 04:07:57 +0530

    anonymous funcs

---
+++ anonymous_func.go
@@ -0,0 +1,21 @@
+// squares returns a function that returns
+// the next square number each time it is called
+package main
+
+import "fmt"
+
+func squares() func() int {
+   var x int
+   return func() int {
+             x++
+             return x * x
+          }
+}
+
+func main() {
+   f :=squares()
+   fmt.Println(f())
+   fmt.Println(f())
+   fmt.Println(f())
+   fmt.Println(f())
+}
