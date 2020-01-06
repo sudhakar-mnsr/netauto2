1615f014735e20ef5aa7d3f71aea4438497241a3
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-01-06 13:38:41 +0530

    varfunc3 modified

--- varfunc3.go
+++ varfunc3.go
@@ -11,6 +11,16 @@ func avg(nums ...float64) float64 {
    return t / float64(n)
 }
 
+func sum(nums ...float64) float64 {
+   var sum float64
+   for _, v := range nums {
+      sum += v
+   }
+   return sum
+}
+
 func main() {
    fmt.Printf("avg([1, 2.5, 3.75]) =%.2f\n", avg(1, 2.5, 3.75))
+   points := []float64{9, 4, 3.7, 7.1, 7.9, 9.2, 10}
+   fmt.Printf("sum(%v) = %.2f\n", points, sum(points...))
 }
