22d3d77a77770de0bdba98c903fd4435442266ea
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-01-06 11:51:48 +0530

    defer example

---
+++ defer.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func do(steps ...string) {
+   defer fmt.Println("All done!")
+   for _, s := range steps {
+      defer fmt.Println(s)
+   }
+   fmt.Println("starting")
+}
+
+func main() {
+   do("Find key", "Aplly break", "Put key in ignition", "Start car",)
+}
