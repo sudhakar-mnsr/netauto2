87a9c432ea9669ec066dfad09b912b9b1de0dcd8
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-01-08 13:02:57 +0530

    added map init

--- arrinit.go
+++ arrinit.go
@@ -8,14 +8,23 @@ func main() {
       fmt.Println(i)
    }
    var truth = [256]bool{true}
-   for _, j := range truth {
-      fmt.Println(j)
+   for _, i := range truth {
+      fmt.Println(i)
    }
    var histogram = [5]map[string]int {
       map[string]int{"A":12, "B":5, "D":11},
       map[string]int{"man":1344, "women":844, "children":577},
    }
-   for _, k := range histogram {
-      fmt.Println(k)
+   for _, i := range histogram {
+      fmt.Println(i)
+   }
+   var board = [4][2]int{ 
+      {33, 23}, 
+      {62, 2}, 
+      {23, 4}, 
+      {51, 88}, 
+   }
+   for _, i := range board {
+      fmt.Println(i)
    }
 }
