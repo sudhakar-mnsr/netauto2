203a22540023f914596c281755ef14d07b8ceb61
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-01-08 14:36:41 +0530

    slice init

--- sliceinit.go
+++ sliceinit.go
@@ -20,4 +20,20 @@ months := []string {
    for _, i := range months {
       fmt.Println(i)
    }
+tables := []map[string][]int {
+         { 
+               "age":{53, 13, 5, 55, 45, 62, 34, 7}, 
+               "pay":{124, 66, 777, 531, 933, 231}, 
+         }, 
+    } 
+   for _, i := range tables {
+      fmt.Println(i)
+   }
+    graph  := [][][][]int{ 
+         {{{44}, {3, 5}}, {{55, 12, 3}, {22, 4}}}, 
+         {{{22, 12, 9, 19}, {7, 9}}, {{43, 0, 44, 12}, {7}}},     
+    }
+   for _, i := range graph {
+      fmt.Println(i)
+   }
 }
