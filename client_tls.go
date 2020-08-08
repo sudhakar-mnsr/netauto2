cf74946dd70b50d7d6d32a0d53c20f8d0dcf0971
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-08 21:03:32 +0530

    examples

--- client_tls.go
+++ client_tls.go
@@ -29,3 +29,45 @@ if err != nil {
    log.Fatal("failed to read CA cert", err)
 }
 
+certPool := x509.NewCertPool()
+certPool.AppendCertsFromPEM(caCert)
+
+// TLS configuration
+tlsConf := &tls.Config{
+            InsecureSkipVerify: false,
+            RootCAs: certPool,
+}
+
+conn, err := tls.Dial(network, addr, tlsConf)
+if err != nil {
+   log.Fatal("failed to create socket:", err)
+}
+defer conn.Close()
+fmt.Println("connected to currency service: ", addr)
+
+var param string
+
+for {
+fmt.Println("Enter search string or *")
+fmt.Print(prompt, "> ")
+_, err = fmt.Scanf("%s", &param)
+if err != nil {
+   fmt.Println("Usage: <search string or *>")
+   continue
+}
+
+req := curr.CurrencyRequest{Get: param}
+
+// Send request:
+// use json encoder to encode value of type curr.CurrencyRequest
+// and stream it to the server via net.Conn.
+if err := json.NewEncoder(conn). Encode(&req); err != nil {
+   switch err := err.(type) {
+   case net.Error:
+      fmt.Println("failed to send request:", err)
+      continue
+   default:
+      fmt.Println("failed to encode request:", err)
+      continue
+   }
+}
