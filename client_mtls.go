86f8931191860eddc7130ff80d43ceb3e264531b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-09 21:09:29 +0530

    examples

--- client_mtls.go
+++ client_mtls.go
@@ -25,3 +25,27 @@ flag.StringVar(&key, "key", "../certs/client-key.pem", "private key")
 flag.StringVar(&ca, "ca", "../certs/ca-cert.pem", "root CA certificate")
 flag.Parse()
 
+caCert, err := ioutil.ReadFile(ca)
+if err != nil {
+   log.Fatal("failed to read CA cert", err)
+}
+
+certPool := x509.NewCertPool()
+certPool.AppendCertsFromPEM(caCert)
+
+// TLS configuration
+tlsConf := &tls.Config{
+            RootCAs: certPool,
+            Certificates: []tls.Certificate{cer},
+}
+
+// create a tls.Conn to connect to server
+conn, err := tls.Dial(network, addr, tlsConf)
+if err != nil {
+   log.Fatal("failed to create socket:", err)
+}
+defer conn.Close()
+fmt.Println("connected to currency service: ", addr)
+
+var param string
+
