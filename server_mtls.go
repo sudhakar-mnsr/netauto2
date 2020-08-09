d2d823cc355e789a9887a98826c97ac04d3ef35f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-09 20:40:29 +0530

    examples

--- server_mtls.go
+++ server_mtls.go
@@ -21,83 +21,83 @@ var (
 )
 
 func main() {
-// setup flags
-var addr, network, cert, key, ca string
-flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
-flag.StringVar(&cert, "cert", "/tmp/certs/cert.pem", "public cert")
-flag.StringVar(&key, "key", "/tmp/certs/key.pem", "private key")
-flag.StringVar(&ca, "ca", "/tmp/certs/cert.pem", "root CA certificate")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp", "tcp4", "tcp6", "unix":
-default:
-	fmt.Println("unsupported network protocol")
-	os.Exit(1)
-}
-
-// load server cert by providing the private key that generated it
-cer, err := tls.LoadX509KeyPair(cert, key)
-if err != nil {
-   log.Fatal(err)
-}
-
-// load root CA
-caCert, err := ioutil.ReadFile(ca)
-if err != nil {
-   log.Fatal(err)
-}
-caPool := x509.NewCertPool()
-caPool.AppendCertsFromPEM(caCert)
-
-// configure tls with certs and other settings
-tlsConfig := &tls.Config{
-              ClientAuth: tls.RequireAndVerifyClientCert,
-              ClientCAs: caPool,
-              Certificates: []tls.Certificate{cer},
-}
-
-// instead of net.Listen, we now use tls.Listen to start a listener 
-// on the secure port
-ln, err := tls.Listen(network, addr, tlsConfig)
-if err != nil {
-   log.Println(err)
-}
-defer ln.Close()
-log.Println("***** Global Currency Service (secure) *****")
-log.Printf("Service started: (%s) %s; server cert %s\n", network, addr, cert)
-
-acceptDelay := time.Millisecond * 10
-acceptCount := 0
-
-for {
-conn, err := ln.Accept()
-if err != nil {
-   switch e := err.(type) {
-   case net.Error:
-      // if temporary error, attempt to connect again
-      if e.Temporary()
-         if acceptCount > 5 {
-            log.Fatalf("unable to connect after %d retries: %v", acceptCount, err)
-         }
-         acceptDelay *= 2
-         acceptCount++
-         time.Sleep(acceptDelay)
-         continue
-      }
+   // setup flags
+   var addr, network, cert, key, ca string
+   flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+   flag.StringVar(&cert, "cert", "/tmp/certs/cert.pem", "public cert")
+   flag.StringVar(&key, "key", "/tmp/certs/key.pem", "private key")
+   flag.StringVar(&ca, "ca", "/tmp/certs/cert.pem", "root CA certificate")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
    default:
-      log.Println(err)
-      if err := conn.Close(); err != nil {
-         log.Fatal(err)
-      }
-      continue
+   	fmt.Println("unsupported network protocol")
+   	os.Exit(1)
    }
-   acceptDelay = time.Millisecond * 10
-   acceptCount = 0
-}
-log.Println("securely connected to remote client ", conn.RemoteAddr())
-go handleConnection(conn)
-}
+   
+   // load server cert by providing the private key that generated it
+   cer, err := tls.LoadX509KeyPair(cert, key)
+   if err != nil {
+      log.Fatal(err)
+   }
+   
+   // load root CA
+   caCert, err := ioutil.ReadFile(ca)
+   if err != nil {
+      log.Fatal(err)
+   }
+   caPool := x509.NewCertPool()
+   caPool.AppendCertsFromPEM(caCert)
+   
+   // configure tls with certs and other settings
+   tlsConfig := &tls.Config{
+                 ClientAuth: tls.RequireAndVerifyClientCert,
+                 ClientCAs: caPool,
+                 Certificates: []tls.Certificate{cer},
+   }
+   
+   // instead of net.Listen, we now use tls.Listen to start a listener 
+   // on the secure port
+   ln, err := tls.Listen(network, addr, tlsConfig)
+   if err != nil {
+      log.Println(err)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service (secure) *****")
+   log.Printf("Service started: (%s) %s; server cert %s\n", network, addr, cert)
+   
+   acceptDelay := time.Millisecond * 10
+   acceptCount := 0
+   
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         switch e := err.(type) {
+         case net.Error:
+            // if temporary error, attempt to connect again
+            if e.Temporary()
+               if acceptCount > 5 {
+                  log.Fatalf("unable to connect after %d retries: %v", acceptCount, err)
+               }
+               acceptDelay *= 2
+               acceptCount++
+               time.Sleep(acceptDelay)
+               continue
+            }
+         default:
+            log.Println(err)
+            if err := conn.Close(); err != nil {
+               log.Fatal(err)
+            }
+            continue
+         }
+         acceptDelay = time.Millisecond * 10
+         acceptCount = 0
+      }
+      log.Println("securely connected to remote client ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
 }
