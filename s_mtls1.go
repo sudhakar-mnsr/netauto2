d0cd12affb1aabc8931c0d7b72fd83e2179ca17d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-10 23:27:46 +0530

    examples

--- s_mtls1.go
+++ s_mtls1.go
@@ -15,28 +15,31 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-// setup a /hello resource handler
-http.HandleFunc("/hello", helloHandler)
-
-// Create a CA certificate pool and add cert.pem to it
-caCert, err := ioutil.ReadFile("cert.pem")
-if err != nil {
-   log.Fatal(err)
+   // setup a /hello resource handler
+   http.HandleFunc("/hello", helloHandler)
+   
+   // Create a CA certificate pool and add cert.pem to it
+   caCert, err := ioutil.ReadFile("cert.pem")
+   if err != nil {
+      log.Fatal(err)
+   }
+   caCertPool := x509.NewCertPool()
+   caCertPool.AppendCertsFromPEM(caCert)
+   
+   // Create the TLS config with the CA pool and enable Client certificate
+   // validation
+   tlsConfig := &tls.Config{
+                 ClientCAs: caCertPool,
+                 ClientAuth: tls.RequireAndVerifyClientCert,
+   }
+   tlsConfig. BuildNameToCertfificate()
+   
+   // Create a server instance to listen on port 8443 with the TLS config
+   server := &http.Server{
+              Addr: ":8443,
+              TLSConfig: tlsConfig,
+   }
+   
+   // Listen to HTTPS connections with the server certificate and wait
+   log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
 }
-caCertPool := x509.NewCertPool()
-caCertPool.AppendCertsFromPEM(caCert)
-
-// Create the TLS config with the CA pool and enable Client certificate
-// validation
-tlsConfig := &tls.Config{
-              ClientCAs: caCertPool,
-              ClientAuth: tls.RequireAndVerifyClientCert,
-}
-tlsConfig. BuildNameToCertfificate()
-
-// Create a server instance to listen on port 8443 with the TLS config
-server := &http.Server{
-           Addr: ":8443,
-           TLSConfig: tlsConfig,
-}
-
