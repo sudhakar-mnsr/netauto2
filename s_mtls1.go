3c4b795d842be5e5c16bcb5bf1782c4735719a9d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-10 23:25:02 +0530

    examples

--- s_mtls1.go
+++ s_mtls1.go
@@ -25,3 +25,18 @@ if err != nil {
 }
 caCertPool := x509.NewCertPool()
 caCertPool.AppendCertsFromPEM(caCert)
+
+// Create the TLS config with the CA pool and enable Client certificate
+// validation
+tlsConfig := &tls.Config{
+              ClientCAs: caCertPool,
+              ClientAuth: tls.RequireAndVerifyClientCert,
+}
+tlsConfig. BuildNameToCertfificate()
+
+// Create a server instance to listen on port 8443 with the TLS config
+server := &http.Server{
+           Addr: ":8443,
+           TLSConfig: tlsConfig,
+}
+
