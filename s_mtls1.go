cdab0e84f69a4a2761e2f1da741ac7d794dbcc93
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-10 23:15:12 +0530

    examples

---
+++ s_mtls1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+   "crypto/tls"
+   "crypto/x509"
+   "io"
+   "io/ioutil"
+   "log"
+   "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+   // Write "Hello, world!" to the response body
+   io.WriteString(w, "Hello, world!\n")
+}
+
