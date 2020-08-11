8830ddc59a9e0883db7b7da89d39d4c9cb8b003f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-12 00:38:38 +0530

    examples

--- dialer_ntp.go
+++ dialer_ntp.go
@@ -45,4 +45,23 @@ if err != nil {
    fmt.Printf("failed to connect: %v\n", err)
    os.Exit(1)
 }
+defer func() {
+   if err := conn.Close(); err != nil {
+      fmt.Println("failed while closing connection:", err)
+   }
+}()
 
+fmt.Printf"time from (%s) (%s)\n", network, conn.RemoteAddr())
+
+// send time request
+if _, err = conn.Write(req); err != nil {
+   fmt.Printf("failed to send request: %v\n", err)
+   os.Exit(1)
+}
+
+// block to receive server response
+read, err := conn.Read(rsp)
+if err != nil {
+   fmt.Printf("failed to receive response: %v\n", err)
+   os.Exit(1)
+}
