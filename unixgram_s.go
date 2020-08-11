b14b4831aa774ef3b2ca34de1b700e89c5ffb573
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-12 01:29:08 +0530

    examples

--- unixgram_s.go
+++ unixgram_s.go
@@ -11,15 +11,37 @@ import (
 // Unix Domain Socket (streaming).  When the server receives a
 // request, it returns its content immediately.
 func main() {
-var addr string
-flag.StringVar(&addr, "e", "/tmp/echo2.sock", "service endpoint address")
-flag.Parse()
-
-// create local unix domain socket address endpoint
-laddr, err := net.ResolveUnixAddr("unix", addr)
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
+   var addr string
+   flag.StringVar(&addr, "e", "/tmp/echo2.sock", "service endpoint address")
+   flag.Parse()
+   
+   // create local unix domain socket address endpoint
+   laddr, err := net.ResolveUnixAddr("unix", addr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // announce service using ListenUnix which creates a UnixListener
+   l, err := net.ListenUnix("unix", laddr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer l.Close()
+   fmt.Println("listening at (unix)", laddr.String())
+   
+   for {
+      // use UnixListener to block and wait for UDS connection request using
+      // AcceptUnix which then creates a UnixConn
+      conn, err := l.AcceptUnix()
+      if err != nil {
+         fmt.Println("failed to accept conn:", err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("connected to: ", conn.RemoteAddr())
+      
+      go handleConnection(conn)
+   }
 }
-
-
