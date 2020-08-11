867ff2dcdc9b1ea4944f094ae57eb2e2e9138be2
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-11 23:31:14 +0530

    examples

--- dialer_curr.go
+++ dialer_curr.go
@@ -72,3 +72,25 @@ fmt.Println("connected to currency service: ", addr)
 
 var param string
 
+for {
+fmt.Print(prompt, "> ")
+_, err = fmt.Scanf("%s", &param)
+if err != nil {
+   fmt.Println("Usage: <search string or *>)
+   continue
+}
+
+req := curr.CurrencyRequest{Get: param}
+// Send request:
+// use json encoder to encode value of type curr.CurrencyRequest
+// and stream it to the server via net.Conn
+if err := json.NewEncoder(conn).Encode(&req); err != nil {
+   switch err := err.(type) {
+   case net.Error:
+      fmt.Println("failed to send request:", err)
+      os.Exit(1)
+   default:
+      fmt.Println("failed to encode request:", err)
+      continue
+   }
+}   
