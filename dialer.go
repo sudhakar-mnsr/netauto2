383d49f2ea36778fba0febec83ac7039f4a98fd8
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-11 23:14:32 +0530

    examples

--- dialer.go
+++ dialer.go
@@ -10,10 +10,19 @@ import (
 )
 
 func main() {
-client := &http.Client{
-           Transport: &http.Transport{
-           DisableKeepAlives: true,
-           Dial: (&net.Dialer{
-              Timeout: 30 * time.Second,
-           },
-}       
+   client := &http.Client{
+              Transport: &http.Transport{
+              DisableKeepAlives: true,
+              Dial: (&net.Dialer{
+                 Timeout: 30 * time.Second,
+              },
+   }       
+   
+   resp, err := client.Get("http://tools.ietf.org/rfc/rfc7540.txt")
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   defer resp.Body.Close()
+   io.Copy(os.Stdout, resp.Body)
+}
