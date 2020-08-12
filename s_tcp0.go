6fe04567fbc51b654557ed1e160b619319b296d5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-12 23:21:34 +0530

    examples

--- s_tcp0.go
+++ s_tcp0.go
@@ -6,3 +6,24 @@ import (
 	"net"
 	"os"
 )
+
+func main() {
+var addr string
+flag.StringVar(&addr, "e", ":4040", "service address endpoint")
+flag.Parse()
+
+// create local addr for socket
+laddr, err := net.ResolveTCPAddr("tcp", addr)
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
+
+// announce services using ListenTCP which is a TCPListener
+l, err := net.ListenTCP("tcp", laddr)
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
+defer l.Close()
+fmt.Println("listening at (tcp)", laddr.String())
