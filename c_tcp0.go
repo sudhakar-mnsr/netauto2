b098a8a51a9343ac036d4e5a23a1fbaa61d2a759
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-12 23:10:26 +0530

    examples

--- c_tcp0.go
+++ c_tcp0.go
@@ -6,3 +6,17 @@ import (
 	"net"
 	"os"
 )
+
+func main() {
+var addr string
+flag.StringVar(&addr, "e", "localhost:4040", "service address endpoint")
+flag.Parse()
+text := flag.Arg(0)
+
+// use ResolveTCPAddr to create address to connect to
+raddr, err := net.ResolveTCPAddr("tcp", addr)
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
+
