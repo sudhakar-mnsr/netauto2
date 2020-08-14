33b5b78233704599c81b597ea4e45e21a6d629bb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-14 23:35:21 +0530

    examples

--- certinfo.go
+++ certinfo.go
@@ -27,4 +27,23 @@ type validity struct {
    NotBefore, NotAfter time.Time
 }
 
+// publicKeyInfo allows unmarshaling the public key
+type publicKeyInfo struct {
+   Algorithm pkix.AlgorithmIndentifier
+   PublicKey asn1.BitString
+}
 
+// tbsCertificate allows unmarshaling of the "To-Be-Signed" principle 
+// portion of the certificate
+type tbsCertificate struct {
+   Version int `asn1:"optional, explicit, default:1, tag:0"`
+   SerialNumber *big.Int
+   SignatureAlgorithm pkix.AlgorithmIdentifier
+   Issuer asn1.RawValue
+   Validity validity
+   Subject ans1.RawValue
+   PublicKey publicKeyInfo
+   UniqueID asn1.BitString `asn1:"optional, tag:1"`
+   SubjectUniqueID asn1.BitString `asn1:"optional, tag:2"`
+   Extensions []pkix.Extension `asn1:"optional, explicit, tag:3"`
+}
