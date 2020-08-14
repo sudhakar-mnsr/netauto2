7cc530ff99a49241e91ffb8bb0e107983ae58b12
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-14 23:55:40 +0530

    examples

--- certinfo.go
+++ certinfo.go
@@ -69,19 +69,37 @@ func certUniqueIDs(tbsAsnData []byte) (issuerUniqueID, subjectUniqueID []byte, e
 // printName prints the fields of a distinguished name, which include such 
 // things as its common name and locality
 func printNames(names []pkix.AttributeTypeAndValue, buf *bytes.Buffer) []string {
-values := []string{}   
-for _, name := range names {
-oid := name.Type
-if len(oid) == 4 && oid[0] == 2 && oid[1] == 5 && oid[2] == 4 {
-switch oid[3] {
-case 3:
-   values = append(values, fmt.Sprintf("CN=%s", name.Value))
-case 6:
-   values = append(values, fmt.Sprintf("C=%s", name.Value))
-case 8:
-   values = append(values, fmt.Sprintf("ST=%s", name.Value))
-case 10:
-   values = append(values, fmt.Sprintf("O=%s", name.Value))
-case 11:
-   values = append(values, fmt.Sprintf("OU=%s", name.Value))
+   values := []string{}   
+   for _, name := range names {
+      oid := name.Type
+      if len(oid) == 4 && oid[0] == 2 && oid[1] == 5 && oid[2] == 4 {
+         switch oid[3] {
+         case 3:
+            values = append(values, fmt.Sprintf("CN=%s", name.Value))
+         case 6:
+            values = append(values, fmt.Sprintf("C=%s", name.Value))
+         case 8:
+            values = append(values, fmt.Sprintf("ST=%s", name.Value))
+         case 10:
+            values = append(values, fmt.Sprintf("O=%s", name.Value))
+         case 11:
+            values = append(values, fmt.Sprintf("OU=%s", name.Value))
+         default: 
+            values = append(values, fmt.Sprintf("UnknownOID=%s", name.Type.String()))
+      }
+   } else if oid.Equal(oidEmailAddress) {
+      values = append(values, fmt.Sprintf("emailAddress=%s", name.Value))
+   } else {
+      values = append(values, fmt.Sprintf("UnknownOID=%s", name.Type.String()))
+   }
+   }
 
+if len(values) > 0 {
+   buf.WriteString(values[0])
+   for i := 1; i < len(values); i++ {
+      buf.WriteString("," + values[i])
+   }
+   buf.WriteString("\n")
+   }
+   return values
+}
