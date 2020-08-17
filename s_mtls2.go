a5fac774d893a7dbced586f89f3e33ba328620f6
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-17 20:19:58 +0530

    examples

--- s_mtls2.go
+++ s_mtls2.go
@@ -13,3 +13,15 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// Write "Hello, world!" to the response body
 	io.WriteString(w, "Hello, world!\n")
 }
+
+func main() {
+// Set up a /hello resource handler
+http.HandleFunc("/hello", helloHandler)
+
+// Create a CA certificate pool and add cert.pem to it
+caCert, err := ioutil.ReadFile("cert.pem")
+if err != nil {
+   log.Fatal(err)
+}
+caCertPool := x509.NewCertPool()
+caCertPool.AppendCertsFromPEM(caCert)
