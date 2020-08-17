6792fb41553ab4d366fcc2df2a76188ffb724db1
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-17 20:33:05 +0530

    examples

--- c_mtls2.go
+++ c_mtls2.go
@@ -15,3 +15,22 @@ cert, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
 if err != nil {
    log.Fatal(err)
 }
+
+// Create a CA certificate pool and add cert.pem to it
+caCert, err := ioutil.ReadFile("cert.pem")
+if err != nil {
+   log.Fatal(err)
+}
+
+caCertPool := x509.NewCertPool()
+caCertPool.AppendCertsFromPEM(caCert)
+
+// Create a HTTPS client and supply the created CA pool and certificate
+client := &http.Client{
+          Transport: &http.Transport{
+             TLSClientConfig: &tls.Config{
+                RootCAs: caCertPool,
+                Certificates: []tls.Certificate{cert},
+             },
+          },
+}
