a98f065156d9bf80a4890eb3367c7f590211f149
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-17 20:25:26 +0530

    examples

--- s_mtls2.go
+++ s_mtls2.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
+   "crypto/tls"
+   "crypto/x509"
+   "io"
+   "io/ioutil"
+   "log"
+   "net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// Write "Hello, world!" to the response body
-	io.WriteString(w, "Hello, world!\n")
+   // Write "Hello, world!" to the response body
+   io.WriteString(w, "Hello, world!\n")
 }
 
 func main() {
-// Set up a /hello resource handler
-http.HandleFunc("/hello", helloHandler)
-
-// Create a CA certificate pool and add cert.pem to it
-caCert, err := ioutil.ReadFile("cert.pem")
-if err != nil {
-   log.Fatal(err)
-}
-caCertPool := x509.NewCertPool()
-caCertPool.AppendCertsFromPEM(caCert)
-
-// Create the TLS config with the CA pool and enable Client certificate
-// validation
-tlsConfig := &tls.Config{
-             ClientCAs: caCertPool,
-             ClientAuth: tls.RequireAndVerifyClientCert,
-}
-tlsConfig.BuildNameToCertificate()
-
-// Create a server instance to listen on port 8443 with the TLS config
-server := &http.Server{
-          Addr: ":8443",
-          TLSConfig: tlsConfig,
-}
-
-// Listen to HTTPS connections with the server certificate and wait
-log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+   // Set up a /hello resource handler
+   http.HandleFunc("/hello", helloHandler)
+   
+   // Create a CA certificate pool and add cert.pem to it
+   caCert, err := ioutil.ReadFile("cert.pem")
+   if err != nil {
+      log.Fatal(err)
+   }
+   caCertPool := x509.NewCertPool()
+   caCertPool.AppendCertsFromPEM(caCert)
+   
+   // Create the TLS config with the CA pool and enable Client certificate
+   // validation
+   tlsConfig := &tls.Config{
+                ClientCAs: caCertPool,
+                ClientAuth: tls.RequireAndVerifyClientCert,
+   }
+   tlsConfig.BuildNameToCertificate()
+   
+   // Create a server instance to listen on port 8443 with the TLS config
+   server := &http.Server{
+             Addr: ":8443",
+             TLSConfig: tlsConfig,
+   }
+   
+   // Listen to HTTPS connections with the server certificate and wait
+   log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
 }
