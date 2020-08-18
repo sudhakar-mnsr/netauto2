cb796d13e4a567d4368ff465a0583ec8ac18adf8
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-18 17:07:16 +0530

    examples

--- lsdns.go
+++ lsdns.go
@@ -16,3 +16,12 @@ var (
    txt bool
    cname bool
 )
+
+func init() {
+	flag.StringVar(&ip, "ip", "", "IP address for DNS operation")
+	flag.StringVar(&host, "host", "", "Host address for DNS operation")
+	flag.BoolVar(&ns, "ns", false, "Host name server lookup")
+	flag.BoolVar(&mx, "mx", false, "Host domain mail server lookup")
+	flag.BoolVar(&txt, "txt", false, "Host domain TXT lookup")
+	flag.BoolVar(&cname, "cname", false, "Host CNAME lookup")
+}
