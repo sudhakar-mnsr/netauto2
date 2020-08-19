f587abd1fd450b18a837b19dbd2027d2201b0a34
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-19 23:21:42 +0530

    examples

--- lsif.go
+++ lsif.go
@@ -9,13 +9,31 @@ import (
 
 // This program lists network interface information
 func main() {
-var ifname string
-if len(os.Args) == 2 {
-   ifname = os.Args[1]
+   var ifname string
+   if len(os.Args) == 2 {
+      ifname = os.Args[1]
+   }
+   ifaces, err := net.Interfaces()
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   for _, iface := range ifaces {
+      if ifname == "" || ifname == iface.Name {
+         addrs, err := iface.Addrs()
+         if err != nil {
+            continue
+         }
+         fmt.Printf("%s: <%s> mtu=%d\n", iface.Name, strings.ToUpper(iface.Flags.String()), iface.MTU)
+         if len(iface.HardwareAddr.String()) > 0 {
+            fmt.Printf("\tether %s\n", iface.HarewareAddr.String())
+         }
+         if len(addrs) > 0 {
+            for _, addr := range addrs {
+               fmt.Printf("\t%s\n", addrInfo(addr))
+            }
+         }
+      }
+   }
 }
-ifaces, err := net.Interfaces()
-if err != nil {
-   fmt.Println(err)
-   os.Exit(1)
-}
-
