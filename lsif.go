23833a447a5245d5119ebde9766e0b6a6ce5e7b1
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-19 23:17:09 +0530

    examples

--- lsif.go
+++ lsif.go
@@ -7,3 +7,15 @@ import (
 	"strings"
 )
 
+// This program lists network interface information
+func main() {
+var ifname string
+if len(os.Args) == 2 {
+   ifname = os.Args[1]
+}
+ifaces, err := net.Interfaces()
+if err != nil {
+   fmt.Println(err)
+   os.Exit(1)
+}
+
