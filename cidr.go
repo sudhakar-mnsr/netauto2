a550752e0d4838f82fbf2aa5c42c3a0743e7da7b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-20 23:43:46 +0530

    examples

--- cidr.go
+++ cidr.go
@@ -19,3 +19,17 @@ func init() {
 // This program implements a CIDR subnet calculator.
 // It takes a CIDR address prefix an calculates ip-ranges,
 // total hosts, wildcard mask, etc.
+
+func main() {
+flag.Parse()
+if cidr == "" {
+   fmt.Println("CIDR address missing")
+   os.Exit(1)
+}
+
+ip, ipnet, err := net.ParseCIDR(cidr)
+if err != nil {
+   fmt.Println("failed parsing CIDR address: ", err)
+   os.Exit(1)
+}
+
