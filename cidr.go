ea2f73e53b7f333fced13de65af45fb92c1ecf93
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-20 23:48:53 +0530

    examples

--- cidr.go
+++ cidr.go
@@ -40,3 +40,9 @@ if err != nil {
 // - Total hosts in the network 2^8
 // Wildcard the inverse of the subnet i.e 0.0.0.255
 // Max address of the subnet 192.168.100.255
+ones, totalBits := ipnet.Mask.Size()
+size := totalBits - ones
+totalHosts := math.Pow(2, float64(size))
+wildcardIP := wildcard(net.IP(ipnet.Mask))
+last := lastIP(ip, net.IPMask(wildcardIP))
+
