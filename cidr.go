7c5a55a378dc835d9f8f3066c2727cdb53f51986
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-20 23:41:51 +0530

    examples

--- cidr.go
+++ cidr.go
@@ -8,3 +8,14 @@ import (
 	"os"
 )
 
+var (
+	cidr string
+)
+
+func init() {
+	flag.StringVar(&cidr, "c", "", "the CIDR address")
+}
+
+// This program implements a CIDR subnet calculator.
+// It takes a CIDR address prefix an calculates ip-ranges,
+// total hosts, wildcard mask, etc.
