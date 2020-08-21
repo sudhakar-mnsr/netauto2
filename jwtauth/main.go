b4259cfafa922af8a8a9711c434a16ae55c2d0d5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-21 23:09:41 +0530

    examples

---
+++ jwtauth/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "time"
+    jwt "github.com/dgrijalva/jwt-go"
+    "github.com/dgrijalva/jwt-go/request"
+    "github.com/gorilla/mux"
+)
