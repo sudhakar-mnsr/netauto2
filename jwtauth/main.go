7ede6c3f523228af4025e6bbf08fcc802f8c1c92
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-21 23:18:33 +0530

    examples

--- jwtauth/main.go
+++ jwtauth/main.go
@@ -25,3 +25,16 @@ func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
    tokenString, err := request.HeaderExtractor{"access_token"}.ExtractToken(r)
    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
       // Dont forget to validate the alg is what you expect:
+      if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+         return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+      }
+      // hmacSampleSecret is a []byte containing your secret, 
+      // e.g []byte("my_secret_key")
+      return secretKey, nil
+   })
+   if err != nil {
+      w.WriteHeader(http.StatusForbidden)
+      w.Write([]byte("Access Denied; Please check the access token"))
+      return
+   }
+
