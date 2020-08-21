f78774251b320db7291c0684635d20348e408352
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-21 23:14:20 +0530

    examples

--- jwtauth/main.go
+++ jwtauth/main.go
@@ -10,3 +10,18 @@ import (
     "github.com/dgrijalva/jwt-go/request"
     "github.com/gorilla/mux"
 )
+
+var secretKey = []byte(os.Getenv("SESSION_SECRET"))
+var users = map[string]string{"naren": "passme", "admin": "password"}
+// Response is a representation of JSON response for JWT
+
+type Response struct {
+   Token string `json: "token"`
+   Status string `json: "status"`
+}
+
+// HEalthcheckHandler returns the date and time
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+   tokenString, err := request.HeaderExtractor{"access_token"}.ExtractToken(r)
+   token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+      // Dont forget to validate the alg is what you expect:
