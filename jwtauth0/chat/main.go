19316d64da8cb43f279902e893a778b4152cdefb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-22 23:53:47 +0530

    examples

--- jwtauth0/chat/main.go
+++ jwtauth0/chat/main.go
@@ -15,7 +15,13 @@ type templateHandler struct {
    templ *template.Template
 }
 
-// 
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   t.once.Do(func() {
+      t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+   })
+   t.templ.Execute(w, nil)
+}
 
 func main() {
    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
