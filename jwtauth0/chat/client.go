5f280bce4cdf579f3e368e07f3810b894519fbf4
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-22 23:59:23 +0530

    examples

--- jwtauth0/chat/client.go
+++ jwtauth0/chat/client.go
@@ -2,3 +2,9 @@ package main
 import (
    "github.com/gorilla/websocket"
 )
+
+type client struct {
+   // socket is the web socket for this client.
+   socket *websocket.Conn
+   // send is the channel on which messages are sent
+
