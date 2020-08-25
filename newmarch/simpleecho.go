3f1c63f02bbbaaa8bc88e4230f5bcc60b451be5d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-25 23:39:25 +0530

    examples

--- newmarch/simpleecho.go
+++ newmarch/simpleecho.go
@@ -25,4 +25,18 @@ func main() {
    }
 }
 
-
+func handleClient(conn net.Conn) {
+   var buf [512]byte
+   for {
+      n, err := conn.Read(buf[0:])
+      if err != nil {
+         return 
+      }
+   
+      fmt.Println("string(buf[0:]))
+      _, err2 := conn.Write(buf[0:n])
+      if err2 != nil {
+         return
+      }
+   }
+}
