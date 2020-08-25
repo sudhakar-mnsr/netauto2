993da052042fd18f63aba00990ff5362ff646fac
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-25 23:35:48 +0530

    examples

--- newmarch/simpleecho.go
+++ newmarch/simpleecho.go
@@ -7,7 +7,22 @@ import (
 )
 
 func main() {
-service := ":1201"
-tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-checkError(err)
+   service := ":1201"
+   tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+   checkError(err)
+   
+   listener, err := net.ListenTCP("tcp", tcpAddr)
+   checkError(err)
+   
+   for {
+   conn, err := listener.Accept()
+   if err != nil {
+      continue
+   }
+   
+   handleClient(conn)
+   conn.Close()
+   }
+}
+
 
