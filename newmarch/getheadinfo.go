bb8b3e194f52980eb5f34fb2c84cf81276572d21
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-25 23:23:35 +0530

    examples

--- newmarch/getheadinfo.go
+++ newmarch/getheadinfo.go
@@ -11,7 +11,25 @@ import (
 )
 
 func main() {
-if len(os.Args) != 2 {
-   fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
-   os.Exit(1)
+   if len(os.Args) != 2 {
+      fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
+      os.Exit(1)
+   }
+   service := os.Args[1]
+   
+   tcpAddr, err := net.ResolveTCPAddr
+   checkError(err)
+   
+   conn, err := net.DialTCP("tcp", nil, tcpAddr)
+   checkError(err)
+   
+   _, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+   checkError(err)
+   
+   result, err := ioutil.ReadAll(conn)
+   checkError(err)
+   
+   fmt.Println(string(result))
+   
+   os.Exit(0)
 }
