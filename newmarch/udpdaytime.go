c34ca106bf75b7e67cf4b98a3609f09ac18199d9
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-26 00:00:00 +0530

    examples

--- newmarch/udpdaytime.go
+++ newmarch/udpdaytime.go
@@ -9,7 +9,19 @@ import (
 )
 
 func main() {
-        if len(os.Args) != 2 {
-                fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
-                os.Exit(1)
-        }
+if len(os.Args) != 2 {
+        fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+        os.Exit(1)
+}
+
+service := os.Args[1]
+
+udpAddr, err := net.ResolveUDPAddr("udp", service)
+checkError(err)
+
+conn, err := net.DialUDP("udp", nil, udpAddr)
+checkError(err)
+
+var buf [512]byte
+n, err := conn.Read(buf[0:])
+
