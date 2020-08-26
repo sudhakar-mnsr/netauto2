6fd5e006204988776a713857b63cca1854b1c652
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-26 13:36:53 +0530

    examples

--- newmarch/threadedEchoServer1.go
+++ newmarch/threadedEchoServer1.go
@@ -22,3 +22,21 @@ func main() {
       go handleClient(conn)
    }
 }
+
+func handleClient(conn net.Conn) {
+   defer conn.Close()
+   
+   var buf [512]byte
+   for {
+      n, err := conn.Read(buf[0:])
+      if err != nil {
+         return
+      }
+      
+      _, err2 := conn.Write(buf[0:n])
+      if err2 != nil {
+         return
+      }
+   }
+}
+
