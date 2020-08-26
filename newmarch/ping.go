4bf9663e6ca75a836ebfb3c66efd5fd68050f337
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-26 14:01:43 +0530

    examples

--- newmarch/ping.go
+++ newmarch/ping.go
@@ -3,6 +3,7 @@
  Prepare ICMP echo command
  Send a ping request to host
  Get a reply
+ NOTE: may need to run as root
 */
 package main
 
@@ -17,3 +18,22 @@ import (
 // set the IP address of your choise
 const myIPAddress = "192.168.1.2"
 const ipv4HeaderSize = 20
+
+func main() {
+if len(os.Args) != 2 {
+   fmt.Println("Usage: ", os.Args[0], "host")
+   os.Exit(1)
+}
+
+localAddr, err := net.ResolveIPAddr("ip4", myIPAddress)
+if err != nil {
+   fmt.Println("Resolution error", err.Error())
+   os.Exit(1)
+}
+
+remoteAddr, err := net.ResolveIPAddr("ip4", os.Args[1])
+if err != nil {
+   fmt.Println("Resolution error", err.Error())
+   os.Exit(1)
+}
+
