591ba1082367eaafa24e6e6324d6212579501cac
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 12:08:53 +0530

    examples

--- newmarch/ftpclient.go
+++ newmarch/ftpclient.go
@@ -105,3 +105,10 @@ func pwdRequest(conn net.Conn) {
    s := string(response[0:n])
    fmt.Println("Current dir \"" + s + "\"")
 }
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Fatal error ", err.Error())
+      os.Exit(1)
+   }
+}
