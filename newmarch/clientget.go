eed1ff9cfbc1b7b5ab5a7804acf4f1af3442de9a
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-28 01:38:44 +0530

    examples

--- newmarch/clientget.go
+++ newmarch/clientget.go
@@ -28,4 +28,12 @@ request, err := http.NewRequest("HEAD", url.String(), nil)
 request.Header.Add("Accept-Charset", "utf-8;q=1, ISO-8859-1;q=0")
 checkError(err)
 
+if response.Status != "200 OK" {
+   fmt.Println(response.Status)
+   os.Exit(2)
+}
+
+fmt.Println("The response header is")
+b, _ := httputil.DumpResponse(response, false)
+fmt.Print(string(b))
 
