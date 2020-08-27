e3e759cf0cb88d1da984d0009c865f9bee5afe07
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 12:04:37 +0530

    examples

--- newmarch/ftpclient.go
+++ newmarch/ftpclient.go
@@ -97,3 +97,11 @@ func cdRequest(conn net.Conn, dir string) {
       fmt.Println("Failed to change dir")
    }
 }
+
+func pwdRequest(conn net.Conn) {
+   conn.Write([]byte(PWD))
+   var response [512]byte
+   n, _ := conn.Read(response[0:n])
+   s := string(response[0:n])
+   fmt.Println("Current dir \"" + s + "\"")
+}
