98c171b59f55ee8876db3856f96e19d09a3d7a2b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 19:59:49 +0530

    examples

--- newmarch/get.go
+++ newmarch/get.go
@@ -12,3 +12,21 @@ import (
         "os"
         "strings"
 )
+
+func main() {
+if len(os.Args) != 2 {
+   fmt.Println("Usage: ", os.Args[0], "host:port")
+   os.Exit(1)
+}
+url := os.Args[1]
+
+response, err := http.Get(url)
+if err != nil {
+   fmt.Println(err.Error())
+   os.Exit(2)
+}
+
+if response.Status != "200 OK" {
+   fmt.Println(response.Status)
+   os.Exit(2)
+}
