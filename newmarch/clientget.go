3d40076ca69d7dca477431c67ee731c88e220f45
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-28 01:36:32 +0530

    examples

--- newmarch/clientget.go
+++ newmarch/clientget.go
@@ -17,4 +17,15 @@ if len(os.Args) != 2 {
 fmt.Println("Usage: ", os.Args[0], "http://host:port/page")
 os.Exit(1)
 }
+url, err := url.Parse(os.Args[1])
+checkError(err)
+
+client := &http.Client{}
+
+request, err := http.NewRequest("HEAD", url.String(), nil)
+
+// only accept UTF-8
+request.Header.Add("Accept-Charset", "utf-8;q=1, ISO-8859-1;q=0")
+checkError(err)
+
 
