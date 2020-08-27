b6a4f3cfbb62bd22af20fda7bf543a354129e9aa
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 19:24:29 +0530

    examples

--- newmarch/head.go
+++ newmarch/head.go
@@ -15,19 +15,22 @@ import (
 )
 
 func main() {
-if len(os.Args) != 2 {
-   fmt.Println("Usage: ", os.Args[0], "host:port")
-   os.Exit(1)
-}
-url := os.Args[1]
-
-response, err := http.Head(url)
-if err != nil {
-   fmt.Println(err.Error())
-   os.Exit(2)
-}
-
-fmt.Println(response.Status)
-for k, v := range response.Header {
-   fmt.Println(k+":", v)
+   if len(os.Args) != 2 {
+      fmt.Println("Usage: ", os.Args[0], "host:port")
+      os.Exit(1)
+   }
+   url := os.Args[1]
+   
+   response, err := http.Head(url)
+   if err != nil {
+      fmt.Println(err.Error())
+      os.Exit(2)
+   }
+   
+   fmt.Println(response.Status)
+   for k, v := range response.Header {
+      fmt.Println(k+":", v)
+   }
+   
+   os.Exit(0)
 }
