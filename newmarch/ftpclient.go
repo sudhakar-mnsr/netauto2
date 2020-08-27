68d87e0b65914d56863b5b10fd45843d92f92172
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 11:13:14 +0530

    examples

--- newmarch/ftpclient.go
+++ newmarch/ftpclient.go
@@ -25,3 +25,23 @@ const (
    CD = "CD"
    PWD = "PWD"
 )
+
+func main() {
+if len(os.Args) != 2 {
+   fmt.Println("Usage: ", os.Args[0], "host")
+   os.Exit(1)
+}
+
+host := os.Args[1]
+
+conn, err := net.Dial("tcp", host+":1202")
+checkError(err)
+
+reader := bufio.NewReader(os.Stdin)
+for {
+line, err := reader.ReadString('\n')
+// lose trailing whitespace
+line = strings.TrimRight(line, " \t\r\n")
+if err != nil {
+   break
+}
