cdddcfbc19e0a31081105c6a84b5bd5b6ba72b12
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 14:59:51 +0530

    examples

--- newmarch/md5hash.go
+++ newmarch/md5hash.go
@@ -15,9 +15,19 @@ import (
 )
 
 func main() {
-hash := md5.New()
-bytes := []byte("hello\n")
-hash.Write(bytes)
-hashValue := hash.Sum(nil)
-hashSize := hash.Size()
-
+   hash := md5.New()
+   bytes := []byte("hello\n")
+   hash.Write(bytes)
+   hashValue := hash.Sum(nil)
+   hashSize := hash.Size()
+   
+   for n := 0; n < hashSize; n += 4 {
+      var val uint32
+      val = uint32(hashValue[n]) << 24 +
+            uint32(hashValue[n+1]) << 16 +
+            uint32(hashValue[n+2]) << 8 +
+            uint32(hashValue[n+3])
+      fmt.Printf("%x ", val)
+   }
+   fmt.Println()
+}
