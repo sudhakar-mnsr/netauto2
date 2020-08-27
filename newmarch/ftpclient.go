03cacc976d25ee309e75e3664a92b3bf8eabdafd
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-27 11:17:36 +0530

    examples

--- newmarch/ftpclient.go
+++ newmarch/ftpclient.go
@@ -27,21 +27,45 @@ const (
 )
 
 func main() {
-if len(os.Args) != 2 {
-   fmt.Println("Usage: ", os.Args[0], "host")
-   os.Exit(1)
-}
-
-host := os.Args[1]
-
-conn, err := net.Dial("tcp", host+":1202")
-checkError(err)
-
-reader := bufio.NewReader(os.Stdin)
-for {
-line, err := reader.ReadString('\n')
-// lose trailing whitespace
-line = strings.TrimRight(line, " \t\r\n")
-if err != nil {
-   break
+   if len(os.Args) != 2 {
+      fmt.Println("Usage: ", os.Args[0], "host")
+      os.Exit(1)
+   }
+   
+   host := os.Args[1]
+   
+   conn, err := net.Dial("tcp", host+":1202")
+   checkError(err)
+   
+   reader := bufio.NewReader(os.Stdin)
+   for {
+      line, err := reader.ReadString('\n')
+      // lose trailing whitespace
+      line = strings.TrimRight(line, " \t\r\n")
+      if err != nil {
+         break
+      }
+      
+      // split into command + arg
+      strs := strings.SplitN(line, " ", 2)
+      // decode user request
+      switch strs[0] {
+      case uiDir:
+         dirRequest(conn)
+      case uiCd:
+         if len(strs) != 2 {
+            fmt.Println("cd <dir>)
+            continue
+         }
+         fmt.Println("CD \"",strs[1], "\"")
+         cdRequest(conn, strs[1])
+      case uiPwd:
+         pwdRequest(conn)
+      case uiQuit:
+         conn.Close()
+         os.Exit(0)
+      default:
+         fmt.Println("Unknown command")
+      }
+   }
 }
