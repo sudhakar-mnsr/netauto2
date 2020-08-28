9c18d020788841cc7792777214f0230a752d26eb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-28 17:45:41 +0530

    examples

--- newmarch/printenv.go
+++ newmarch/printenv.go
@@ -20,3 +20,19 @@ func main() {
    err := http.ListenAndServe(":8080", nil)
    checkError(err)
 }
+
+func printEnv(writer http.ResponseWriter, req *http.Request) {
+   env := os.Environ()
+   writer.Write([]byte("<h1>Environment</h1>\n<pre>"))
+   for _, v := range env {
+      writer.Write([]byte(v + "\n"))
+   }
+   writer.Write([]byte("</pre>"))
+}
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Fatal error ", err.Error())
+      os.Exit(1)
+   }
+}
