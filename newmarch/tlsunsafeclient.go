e14999adf7a05bf5719e59b412ce68848727189d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-28 17:03:31 +0530

    examples

--- newmarch/tlsunsafeclient.go
+++ newmarch/tlsunsafeclient.go
@@ -18,43 +18,52 @@ import (
 )
 
 func main() {
-if len(os.Args) != 2 {
-   fmt.Println("Usage: ", os.Args[0], "https://host:port/page")
-   os.Exit(1)
+   if len(os.Args) != 2 {
+      fmt.Println("Usage: ", os.Args[0], "https://host:port/page")
+      os.Exit(1)
+   }
+   url, err := url.Parse(os.Args[1])
+   checkError(err)
+   
+   if url.Scheme != "https" {
+      fmt.Println("Not https scheme ", url.Scheme)
+      os.Exit(1)
+   }
+   
+   transport := &http.Transport{}
+   transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+   client := &http.Client{Transport: transport}
+   
+   request, err := http.NewRequest("GET", url.String(), nil)
+   // only accept UTF-8
+   checkError(err)
+   
+   response, err := client.Do(request)
+   checkError(err)
+   
+   if response.Status != "200 OK" {
+      fmt.Println(response.Status)
+      os.Exit(2)
+   }
+   fmt.Println("get a response")
+   
+   chSet := getCharset(response)
+   fmt.Printf("got charset %s\n", chSet)
+   if chSet != "UTF-8" {
+      fmt.Println("Cannot handle", chSet)
+      os.Exit(4)
+   }
+   
+   var buf [512]byte
+   reader := response.Body
+   fmt.Println("got body")
+   
+   for {
+   n, err := reader.Read(buf[0:])
+   if err != nil {
+      os.Exit(0)
+   }
+   fmt.Print(string(buf[0:n]))
+   }
+   os.Exit(0)
 }
-url, err := url.Parse(os.Args[1])
-checkError(err)
-
-if url.Scheme != "https" {
-   fmt.Println("Not https scheme ", url.Scheme)
-   os.Exit(1)
-}
-
-transport := &http.Transport{}
-transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-client := &http.Client{Transport: transport}
-
-request, err := http.NewRequest("GET", url.String(), nil)
-// only accept UTF-8
-checkError(err)
-
-response, err := client.Do(request)
-checkError(err)
-
-if response.Status != "200 OK" {
-   fmt.Println(response.Status)
-   os.Exit(2)
-}
-fmt.Println("get a response")
-
-chSet := getCharset(response)
-fmt.Printf("got charset %s\n", chSet)
-if chSet != "UTF-8" {
-   fmt.Println("Cannot handle", chSet)
-   os.Exit(4)
-}
-
-var buf [512]byte
-reader := response.Body
-fmt.Println("got body")
-
