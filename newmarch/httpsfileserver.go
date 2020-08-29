e54cde8fc281324d66949d66edd5bb63d9d05826
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-29 22:56:56 +0530

    examples

--- newmarch/httpsfileserver.go
+++ newmarch/httpsfileserver.go
@@ -23,3 +23,10 @@ fileServer := http.FileServer(http.Dir("/var/www"))
 err := http.ListenAndServeTLS(":8000", "jan.mewmarch.name.pem", "private.pem", fileServer)
 checkError(err)
 }
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Fatal error ", err.Error())
+      os.Exit(1)
+   }
+}
