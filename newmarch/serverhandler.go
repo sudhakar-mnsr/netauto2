702f3bc7946e87b8e405158548a18a4437673952
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-29 22:44:02 +0530

    examples

--- newmarch/serverhandler.go
+++ newmarch/serverhandler.go
@@ -8,3 +8,18 @@
    If you want to takeover the multiplexer role then you can give-
    ca non-nil function as the handler function to ListenAndServe.
 */
+
+package main
+
+import (
+   "net/http"
+)
+
+func main() {
+   myHandler := http.HandlerFunc(func(rw http.ResponseWriter, request *http.Request)
+   // just return no content - arbitrary headers can be set, arbitrary body
+   rw.WriteHeader(http.StatusNoContent)
+   })
+   
+   http.ListenAndServe(":8080", myHandler)
+}  
