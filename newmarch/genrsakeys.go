2e82a6dc8c355fbe59fc2e358b01d92cbefb9f59
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-29 23:09:07 +0530

    examples

--- newmarch/genrsakeys.go
+++ newmarch/genrsakeys.go
@@ -14,15 +14,20 @@ import (
 )
 
 func main() {
-reader := rand.Reader
-bitSize := 512
-key, err := rsa.GenerateKey(reader, bitSize)
-checkError(err)
-
-fmt.Println("Private key primes", key.Primes[0].String(), key.Primes[1].String())
-fmt.Println("Private key exponent", key.D.String())
-
-publicKey := key.PublicKey
-fmt.Println("Public key modulus", publicKey.N.String())
-fmt.Println("Public key exponent", publicKey.E)
-
+   reader := rand.Reader
+   bitSize := 512
+   key, err := rsa.GenerateKey(reader, bitSize)
+   checkError(err)
+   
+   fmt.Println("Private key primes", key.Primes[0].String(), key.Primes[1].String())
+   fmt.Println("Private key exponent", key.D.String())
+   
+   publicKey := key.PublicKey
+   fmt.Println("Public key modulus", publicKey.N.String())
+   fmt.Println("Public key exponent", publicKey.E)
+   
+   saveGobKey("private.key", key)
+   saveGobKey("public.key", publicKey)
+   
+   savePEMKey("private.pem", key)
+}
