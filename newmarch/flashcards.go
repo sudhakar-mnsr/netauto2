25ee9d7c23eba09c417a6afe632ccbaf5c0359df
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-30 02:58:30 +0530

    examples

--- newmarch/flashcards.go
+++ newmarch/flashcards.go
@@ -132,3 +132,8 @@ func listWords(rw http.ResponseWriter, cardname string) {
    fmt.Println("No error")
 }
 
+func checkError(err error) {
+if err != nil {
+   fmt.Println("Fatal error", err.Error())
+   os.Exit(1)
+}
