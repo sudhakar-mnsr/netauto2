b4c9bc0c3d9f0260785e07dae8f317b1cf90d760
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-29 23:19:55 +0530

    examples

--- newmarch/loadrsakeys.go
+++ newmarch/loadrsakeys.go
@@ -11,7 +11,15 @@ import (
 )
 
 func main() {
-var key rsa.PrivateKey
-loadKey("private.key", &key)
-
-
+   var key rsa.PrivateKey
+   loadKey("private.key", &key)
+   
+   fmt.Println("Private key primes", key.Primes[0].String(), key.Primes[1].String())
+   fmt.Println("Private key exponent", key.D.String())
+   
+   var publicKey rsa.PublicKey
+   loadKey("public.key", &publicKey)
+   
+   fmt.Println("Public key modulus", publicKey.N.String())
+   fmt.Println("Public key exponent", publicKey.E)
+}
