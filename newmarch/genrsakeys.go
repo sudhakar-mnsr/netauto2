8381ee9f84463fe130077a7593eab47c0f457e2b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-29 23:34:32 +0530

    examples

--- newmarch/genrsakeys.go
+++ newmarch/genrsakeys.go
@@ -52,7 +52,7 @@ func savePEMKey(fileName string, key *rsa.PrivateKey) {
    outFile.Close()
 }
 
-func checkError(err Error) {
+func checkError(err error) {
    if err != nil {
       fmt.Println("Fatal error", err.Error())
       os.Exit(1)
