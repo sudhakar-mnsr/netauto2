b7fb925a233ec465910cea5b3d529b672d970fc5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-30 22:03:05 +0530

    examples

--- newmarch/echoserver_ws.go
+++ newmarch/echoserver_ws.go
@@ -32,3 +32,16 @@ func Echo(ws *websocket.Conn) {
       fmt.Println("Received back from client: " + reply)
    }
 }
+
+func main() {
+   http.Handle("/", websocket.Handler(Echo))
+   err := http.ListenAndServe(":12345", nil)
+   checkError(err)
+}
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Fatal error ", err.Error())
+      os.Exit(1)
+   }
+}
