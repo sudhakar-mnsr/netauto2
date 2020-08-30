8bebe991834ee1f775f760ee22ee8d6a65e01e50
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-30 22:00:39 +0530

    examples

--- newmarch/echoserver_ws.go
+++ newmarch/echoserver_ws.go
@@ -11,9 +11,24 @@ import (
 )
 
 func Echo(ws *websocket.Conn) {
-fmt.Println("Echoing")
-
-for n := 0; n < 10; n++ {
-msg := "Hello " + string(n+48)
-fmt.Println("Sending to client: " + msg)
-err := websocket.Message.Send(ws, msg)
+   fmt.Println("Echoing")
+   
+   for n := 0; n < 10; n++ {
+      msg := "Hello " + string(n+48)
+      fmt.Println("Sending to client: " + msg)
+      
+      err := websocket.Message.Send(ws, msg)
+      if err != nil {
+         fmt.Println("Cant send")
+         break
+      } 
+      
+      var reply string
+      err = websocket.Message.Receive(ws, &reply)
+      if err != nil {
+         fmt.Println("Cant receive")
+         break
+      } 
+      fmt.Println("Received back from client: " + reply)
+   }
+}
