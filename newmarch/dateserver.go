cc8e5d51db52d3fc0453ea38814614ee1d1dbeaf
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-08-31 21:42:29 +0530

    examples

--- newmarch/dateserver.go
+++ newmarch/dateserver.go
@@ -15,7 +15,22 @@ import (
 var ROOT_DIR = "/home/httpd/html/golang-hidden/websockets"
 
 func GetTemp(ws *websocket.Conn) {
-for {
-msg, _ := exec.Command("date").Output()
-fmt.Println("Sending to client: " + string(msg[:]))
-err := websocket.Message.Send(ws, string(msg[:]))
+   for {
+      msg, _ := exec.Command("date").Output()
+      fmt.Println("Sending to client: " + string(msg[:]))
+      err := websocket.Message.Send(ws, string(msg[:]))
+      if err != nil {
+         fmt.Println("Cant send")
+         break
+      }
+      time.Sleep(2 * 1000 * 1000 * 1000)
+      
+      var reply string
+      err = websocket.Message.Receive(ws, &reply)
+      if err != nil {
+         fmt.Println("Cant receive")
+         break
+      }
+      fmt.Println("Received back from client: " + reply)
+   }
+}
