5d54ab9588f6c721d647a83b500c8d053ab6ac94
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-01 19:45:48 +0530

    examples

--- newmarch/server_rpc.go
+++ newmarch/server_rpc.go
@@ -18,3 +18,11 @@ type Values struct {
 type Quotient struct {
 	Quo, Rem int
 }
+
+type Arith int
+
+func (t *Arith) Multiply(args *Values, reply *int) error {
+   *reply = args.A * args.B
+   return nil
+}
+
