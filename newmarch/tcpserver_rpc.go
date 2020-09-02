7caa6dd5492f30faa6767d1ada00d7a5e8d638d6
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-02 20:02:51 +0530

    examples

--- newmarch/tcpserver_rpc.go
+++ newmarch/tcpserver_rpc.go
@@ -19,3 +19,19 @@ type Args struct {
 type Quotient struct {
 	Quo, Rem int
 }
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+   *reply = args.A * args.B
+   return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+   if args.B == 0 {
+      return errors.New("divide by zero")
+   }
+   quo.Quo = args.A / args.B
+   quo.Rem = args.A % args.B
+   return nil
+}
