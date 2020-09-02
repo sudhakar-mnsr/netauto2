369623be8e70d0deaccaf2d2f90c0131ef062aeb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-02 19:56:02 +0530

    examples

--- newmarch/tcpclient_rpc.go
+++ newmarch/tcpclient_rpc.go
@@ -30,3 +30,17 @@ client, err := rpc.Dial("tcp", service)
 if err != nil {
    log.Fatal("dialing:", err)
 }
+
+// Synchronous call
+args := Args{17, 8}
+var reply int
+err = client.Call("Arith.Multiply", args, &reply)
+if err != nil {
+   log.Fatal("arith error:", err)
+}
+fmt.Printf("Arith: %d*%d=%d\n", args, &quot)
+if err != nil {
+   log.Fatal("arith error:", err)
+}
+fmt.Printf("Arith: %d/%d=%d remainder %d\n", args.A, args.B, quot.Quo, quot.Rem)
+}
