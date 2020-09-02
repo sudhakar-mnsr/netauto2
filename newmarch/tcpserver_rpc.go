ddd374c73a583d9aac28df6ab66c33b7eb262669
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-02 20:05:40 +0530

    examples

--- newmarch/tcpserver_rpc.go
+++ newmarch/tcpserver_rpc.go
@@ -37,12 +37,20 @@ func (t *Arith) Divide(args *Args, quo *Quotient) error {
 }
 
 func main() {
-arith := new(Arith)
-rpc.Register(arith)
-
-tcpAddr, err := net.ResolveTCPAddr("tcp", ":1234")
-checkError(err)
-
-listener, err := net.ListenTCP("tcp", tcpAddr)
-checkError(err)
-
+   arith := new(Arith)
+   rpc.Register(arith)
+   
+   tcpAddr, err := net.ResolveTCPAddr("tcp", ":1234")
+   checkError(err)
+   
+   listener, err := net.ListenTCP("tcp", tcpAddr)
+   checkError(err)
+   
+   for {
+      conn, err := listener.Accept()
+      if err != nil {
+         continue
+      }
+      rpc.ServeConn(conn)
+   }
+}
