a0f204251010b77133ece7151750830f86cca37c
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-03 22:54:00 +0530

    examples

--- gheorghe/iptables.sh
+++ gheorghe/iptables.sh
@@ -17,3 +17,14 @@ $IP t nat A POSTROUTING s 192.168.1.50 j SNAT -to 1.2.4.1
 #Snat Everyone
 $IP t nat A POSTROUTING s 192.168.1.0/24 o eth1 j SNAT -to 1.2.4.0-1.2.4.32 -to 1.2.3.1
 ################### DNAT PART
+$IP t nat A PREROUTING d 1.2.4.1 j DNAT -to 192.168.1.50
+#DNAT the intranet server for the guy in the financial department
+$IP t nat A PREROUTING s 1.2.5.17 d 1.2.4.2 p tcp -dport 65521 j DNAT to 192.168.1.100:22
+#DNAT the web server 
+$IP t nat A PREROUTING d 1.2.4.5 p tcp -dport 80 j DNAT -to 192.168.1.200
+################## Transparent Proxy
+#Allow the boss to bypass the proxy server
+$IP t nat A PREROUTING s 192.168.1.50 p tcp -dport 80 j ACCEPT
+#Do transparent proxy for the rest of the people
+$IP t nat A PREROUTING s 192.168.1.0/24 p tp -dport 80 j REDIRECT -to-port 3128
+### End of NAT section
