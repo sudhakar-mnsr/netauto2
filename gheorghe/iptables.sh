f9b1769b35385b082d15a8b40cb9ef2d020cfbd7
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-03 22:33:24 +0530

    examples

--- gheorghe/iptables.sh
+++ gheorghe/iptables.sh
@@ -11,3 +11,9 @@ $IP t nat F
 $IP t nat A POSTROUTING s 192.168.1.31 p tcp -dport ! 80 j DROP
 $IP t nat A POSTROUTING s 192.168.1.31 p udp -dport ! 53 j DROP
 #Dont SNAT anything from 192.168.1.0/24 to 192.168.2.0/24
+$IP t nat A POSTROUTING s 192.168.1.0/24 d 192.168.2.0/24 j ACCEPT
+#The boss needs DNAT but we should also SNAT her IP address to 1.2.4.1
+$IP t nat A POSTROUTING s 192.168.1.50 j SNAT -to 1.2.4.1
+#Snat Everyone
+$IP t nat A POSTROUTING s 192.168.1.0/24 o eth1 j SNAT -to 1.2.4.0-1.2.4.32 -to 1.2.3.1
+################### DNAT PART
