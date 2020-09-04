802654b724347fc01cebc7743956c759d677caef
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-04 23:57:28 +0530

    examples

--- gheorghe/QoS.sh
+++ gheorghe/QoS.sh
@@ -28,3 +28,14 @@ tc class add dev eth2 parent 20:10 classid 20:200 cbq bandwidth 100Mbit rate \
 1536Kbit allot 1514 weight 192Kbit prio 5 maxburst 20 avpkg 1000
 tc qdisc add dev eth2 parent 20:200 sfq quantum 1514b perturb 15
 tc filter add dev eth2 parent 20:0 protocol ip prio 5 u32 match ip dst 1.1.2.64/27 flowid
+#delete root qdisc for eth1
+tc qdisc del dev eth1 root
+#attach rot qdisc and create the root class for eth1
+tc qdisc add dev eth1 root handle 10: cbq bandwidth 100Mbit avpkt 1000
+tc class add dev eth1 parent 10:0 classid 10:1 cbq bandwidth 100Mbit rate \
+100Mbit allot 1514 weight 10Mbit prio 8 maxburst 20 avpkt 1000
+#create the 1Mbps class for the web and mail server
+tc class add dev eth1 parent 10:1 classid 10:100 cbq bandwidth 100Mbit rate \
+1Mbit allot 1514 weight 128Kbit prio 5 maxburst 20 avpkt 1000 bounded
+tc qdisc add dev eth1 parent 10:100 sfq quantum 1514b perturb 15
+tc filter add dev eth1 parent 10:0 protocol ip prio 5 u32 match ip dst 1.1.2.2 flowid 10:
