71d6248f34a33bd5e3eff69593b8fef462d27ea1
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-05 12:02:53 +0530

    examples

--- gheorghe/setup_hypermarket.sh
+++ gheorghe/setup_hypermarket.sh
@@ -36,9 +36,13 @@
 #3) IP ATA must have static private IP 192.168.2.3
 ####################### END STORE A ######################
 ####################### BEGIN STORE B&C ######################
-#1) The provider assigned private IP address 10.10.12.1 for MAN connection.
-#2) This is 100 Mbps metropolitan access and no internet access
-#3) We set Linux router A LAN interfaces with private IP 192.168.2.1
-#4) We set MS SQL with static private IP 192.168.2.2
-#3) IP ATA must have static private IP 192.168.2.3
+#1) The provider assigned public IP 1.1.2.1 for B and 1.1.3.1 for C
+#2) The connections are 2 Mbps for B and 1 Mbps for C
+#3) We use private class 192.168.3.0/24 and 192.168.4.0/24 for B&C resp.
+#3) We set Linux routers B&C LAN interfaces with private IPs 192.168.3.1 & 192.168.4.0/24.
+#4) We set MS SQL with static private IPs 192.168.3.2(B) & 192.168.4.2(C)
+#3) IP ATA must have static private IPs 192.168.3.3 & 192.168.3.4
 ####################### END STORE B&C ######################
+#NOTE: All remote locations must have an encrypted VPN connection to headquarters
+#We are using IP tunnel for VPN connection
+#Linux distro is Debian
