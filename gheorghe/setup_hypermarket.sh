17758b6cfb3600a958784615f0de6db862c17787
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-05 11:42:00 +0530

    examples

--- gheorghe/setup_hypermarket.sh
+++ gheorghe/setup_hypermarket.sh
@@ -21,3 +21,24 @@
 #The other stores have internet connections from other ISP's in the -
 #cities they are in.
 
+####################### BEGIN HEADQUARTERS ######################
+#1) Provider assigned public IP 1.1.1.1 for internet connection, this 10Mbps internet connection and 100Mbps metropolitan access
+#2) We decided to use private class C 192.168.1.0/24 for internal network.
+#3) We set HW router LAN interface with private IP 192.168.1.1
+#4) We set MS SQL with static private IP 192.168.1.2
+#3) IP ATA must have static private IP 192.168.1.3
+####################### END HEADQUARTERS ######################
+####################### BEGIN STORE A ######################
+#1) The provider assigned private IP address 10.10.12.1 for MAN connection.
+#2) This is 100 Mbps metropolitan access and no internet access
+#3) We set Linux router A LAN interfaces with private IP 192.168.2.1
+#4) We set MS SQL with static private IP 192.168.2.2
+#3) IP ATA must have static private IP 192.168.2.3
+####################### END STORE A ######################
+####################### BEGIN STORE B&C ######################
+#1) The provider assigned private IP address 10.10.12.1 for MAN connection.
+#2) This is 100 Mbps metropolitan access and no internet access
+#3) We set Linux router A LAN interfaces with private IP 192.168.2.1
+#4) We set MS SQL with static private IP 192.168.2.2
+#3) IP ATA must have static private IP 192.168.2.3
+####################### END STORE B&C ######################
