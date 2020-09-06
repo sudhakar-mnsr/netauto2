b45b146dccb97ab73c2f153056768fa516d2d2df
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-07 01:27:29 +0530

    examples

--- gheorghe/iptables_routers.sh
+++ gheorghe/iptables_routers.sh
@@ -14,4 +14,24 @@ $I -N mang
 #Lo + dns + icmp + !syn
 $I -A INPUT -i lo -j ACCEPT
 
+$I -A INPUT -p tcp ! --syn -j ACCEPT
+$I -A INPUT -p icmp -j ACCEPT
+$I -A INPUT -p UDP --sport 53 -s our.dns.server.ourcompany.org -j ACCEPT
+$I -A INPUT -p UDP --sport 53 -s our.dns.server2.ourcompany.org -j ACCEPT
+#Management chain - ssh, snmp, zebra, BGPD
+$I -A INPUT -p TCP --dport 22 -j manag #ssh
+$I -A INPUT -p UDP --dport 161 -j manag #snmp
+$I -A INPUT -p TCP --dport 2601 -j manag #zebra
+$I -A INPUT -p TCP --dport 2605 -j manag #bgpd
+#Network Administrators IP address
+$I -A manag -s 1.1.40.0/27 -j ACCEPT
+$I -A manag -s 1.1.169.o/27
+$I -A manag -j DROP
 
+#Our BGP peers
+$I -A INPUT -s 1.3.2.1 -p TCP --dport 179 -j ACCEPT #provider-2 peering
+$I -A INPUT -s 1.3.1.89 -p TCP --dport 179 -j ACCEPT #provider-2 internet
+$I -A INPUT -s 1.1.1.190 -p TCP --dport  179 -j ACCEPT #core-1
+$I -A INPUT -s 1.1.1.22 -p TCP --dport  179 -j ACCEPT #core-4
+#policy DROP for INPUT chain
+$I -p INPUT DROP
