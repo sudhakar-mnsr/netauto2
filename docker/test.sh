5816f22a34c4dc3a1b7c21e7157e25b683be9113
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-08 23:13:21 +0530

    examples

--- docker/test.sh
+++ docker/test.sh
@@ -4,3 +4,15 @@ ifdown eth1 && sudo ifup eth1
 ip addr show dev eth1
 ip address add 172.16.10.65/26 dev eth2
 ip link set eth2 up
+=========================================================
+#                          NET2                         #
+=========================================================
+ip link add dummy0 type dummy
+ip address add 172.16.10.129/26 dev dummy0
+ip link set dummy0 up
+=========================================================
+#                          NET3                         #
+=========================================================
+ip link add dummy0 type dummy
+ip address add 172.16.10.193/26 dev dummy0
+ip link set dummy0 up
