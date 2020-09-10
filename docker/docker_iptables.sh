492ad5d316e4fef70cc42df3bcb438cc5b12092e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-11 04:16:00 +0530

    examples

--- docker/docker_iptables.sh
+++ docker/docker_iptables.sh
@@ -2,3 +2,9 @@ user@docker1:~$ sudo iptables -A INPUT -i eth0 -p tcp --dport 22 \
 -m state --state NEW,ESTABLISHED -j ACCEPT
 user@docker1:~$ sudo iptables -A OUTPUT -o eth0 -p tcp --sport 22 \
 -m state --state ESTABLISHED -j ACCEPT
+
+#Allow container inbound and outbound access
+sudo iptables -A FORWARD -i docker0 ! \
+-o docker0 -j ACCEPT
+sudo iptables -A FORWARD -o docker0 \
+-m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
