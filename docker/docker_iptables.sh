f1cf5945f1b0767763e62256a37e47bbf96e529b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-11 04:19:15 +0530

    examples

--- docker/docker_iptables.sh
+++ docker/docker_iptables.sh
@@ -12,3 +12,12 @@ sudo iptables -A FORWARD -o docker0 \
 #masquerade container traffic
 sudo iptables -t nat -A POSTROUTING \
 -s 172.17.0.0/16 ! -o docker0 -j MASQUERADE
+
+# Allow inter container communication
+iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
+
+#Allow publish of ports
+sudo iptables -t nat -A PREROUTING ! -i docker0 \
+-p tcp -m tcp --dport 32768 -j DNAT --to-destination 172.17.0.2:80
+sudo iptables -t nat -A PREROUTING ! -i docker0 \
+-p tcp -m tcp --dport 32769 -j DNAT --to-destination 172.17.0.3:80
