5c131909788d7853bb1751e7886e4cc7ba2827be
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-11 04:17:28 +0530

    examples

--- docker/docker_iptables.sh
+++ docker/docker_iptables.sh
@@ -8,3 +8,7 @@ sudo iptables -A FORWARD -i docker0 ! \
 -o docker0 -j ACCEPT
 sudo iptables -A FORWARD -o docker0 \
 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+#masquerade container traffic
+sudo iptables -t nat -A POSTROUTING \
+-s 172.17.0.0/16 ! -o docker0 -j MASQUERADE
