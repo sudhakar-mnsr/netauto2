7c74f3b50a67e41a0433ee44eae311e1a94d17a5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-13 01:35:11 +0530

    examples

--- rick_morty/http.go
+++ rick_morty/http.go
@@ -11,17 +11,36 @@ import (
 )
 
 func makePetition(options map[string]interface{}) (interface{}, error) {
-client := &http.Client{
-          Timeout: 5 * time.Second,
-}
-
-endpoint := ""
-hasParams := false
-
-for k, v := range options {
-switch v.(type) {
-case string:
-   if k == "endpoint" {
-      endpoint = v.(string)
+   client := &http.Client{
+             Timeout: 5 * time.Second,
    }
+   
+   endpoint := ""
+   hasParams := false
+   
+   for k, v := range options {
+   switch v.(type) {
+   case string:
+      if k == "endpoint" {
+         endpoint = v.(string)
+      }
+   
+   case map[string]string:
+      if k == "params" {
+         hasParams = true
+      }
+   case map[string]int:
+      if k == "params" {
+         integer := strconv.FormatInt(int64(v.(map[string]int)["integer"]), 10)
+         endpoint = endpoint + integer
+         delete(options, "params")
+      }
+   case []int:
+      params := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(v.([]int))), ","), "[]")
+      endpoint = endpoint + params
+   default: 
+      err := sliceIntToString(v.([]int), ",")
+      return nil, errors.New(err)
+   }
+}
 
