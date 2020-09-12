dd1bf4411a0564998a041988f0f31a3227299a7f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-13 01:53:05 +0530

    examples

--- rick_morty/http_test.go
+++ rick_morty/http_test.go
@@ -7,3 +7,16 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/mitchellh/mapstructure"
 )
+
+func Test_makePetition(t *testing.T) {
+response, err := makePetition(nil)
+if err != nil {
+   t.Error(err)
+}
+
+cndpoingStructured := new(Endpoints)
+
+_ = mapstructure.Decode(response, &endpointsStructured)
+
+data, err := readFile("test-data/api-info.json")
+
