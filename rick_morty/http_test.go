a3a9d52de62687e0015f85376576d36af43d5385
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-13 01:56:43 +0530

    examples

--- rick_morty/http_test.go
+++ rick_morty/http_test.go
@@ -9,19 +9,29 @@ import (
 )
 
 func Test_makePetition(t *testing.T) {
-response, err := makePetition(nil)
-if err != nil {
-   t.Error(err)
+   response, err := makePetition(nil)
+   if err != nil {
+      t.Error(err)
+   }
+   
+   cndpoingStructured := new(Endpoints)
+   
+   _ = mapstructure.Decode(response, &endpointsStructured)
+   
+   data, err := readFile("test-data/api-info.json")
+   if err != nil {
+      t.Error(err)
+   }
+   
+   endpoints := new(Endpoints)
+   json.Unmarshal(data, &endpoints)
+   
+   comparation := cmp.Equal(endpoints, endpointsStructured)
+   
+   if !comparation {
+      t.Error("The response from makePetition was:")
+      t.Error(endpointsStructured)
+      t.Error("The data against is being run this test is:")
+      t.Error(endpoints)
+   }
 }
-
-cndpoingStructured := new(Endpoints)
-
-_ = mapstructure.Decode(response, &endpointsStructured)
-
-data, err := readFile("test-data/api-info.json")
-if err != nil {
-   t.Error(err)
-}
-
-endpoints := new(Endpoints)
-json.Unmarshal(data, &endpoints)
