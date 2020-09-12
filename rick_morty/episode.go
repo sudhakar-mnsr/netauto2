d3201e6bc2e0c543cd229a8fbde810ff1cd5b5ea
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-12 18:38:45 +0530

    examples

--- rick_morty/episode.go
+++ rick_morty/episode.go
@@ -15,3 +15,20 @@ if options == nil {
       "endpoint": endpoint,
    }
 }
+
+for k, v := range options {
+switch v.(type) {
+case int:
+   if k == "page" {
+      hasParams = true
+      params[k] = strconv.FormatInt(int64(v.(int)), 10)
+   }
+   delete(options, k)
+case string:
+if k == "endpoint" {
+   continue
+}
+// Valid parameters to be passed to the parameters map
+validParams := []string{"name", "status", "species", "type", "gender"}
+exists := containsString(validParams, k)
+
