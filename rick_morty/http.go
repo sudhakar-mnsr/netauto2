74d8d6f1d4eda476e8b43bfb16030e93bee52a55
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-13 01:29:45 +0530

    examples

--- rick_morty/http.go
+++ rick_morty/http.go
@@ -9,3 +9,19 @@ import (
    "strings"
    "time"
 )
+
+func makePetition(options map[string]interface{}) (interface{}, error) {
+client := &http.Client{
+          Timeout: 5 * time.Second,
+}
+
+endpoint := ""
+hasParams := false
+
+for k, v := range options {
+switch v.(type) {
+case string:
+   if k == "endpoint" {
+      endpoint = v.(string)
+   }
+
