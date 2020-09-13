9d7beee16a3f13f45fbf25ac3c02ddd14bb29d82
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-14 02:50:07 +0530

    examples

--- rick_morty/helpers_test.go
+++ rick_morty/helpers_test.go
@@ -14,3 +14,20 @@ var slice = []string{
 var sliceIntegers = []int{
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 }
+
+func Test_containsStringElementPresent(t *testing.T) {
+   sliceToIterate := []string{
+      "hello",
+      "hola",
+      "Allo",
+      "world",
+      "mundo",
+      "Welt",
+   }
+   
+   for _, element := range sliceToIterate {
+      if !containsString(slice, element) {
+         t.Errorf("The %s (%T) is not present in %v", element, element, slice)
+      }
+   }
+}
