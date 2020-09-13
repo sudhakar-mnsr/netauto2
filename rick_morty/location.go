06a1be0f7a4310d1d9b67167e7673171f968f002
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-14 03:01:29 +0530

    examples

--- rick_morty/location.go
+++ rick_morty/location.go
@@ -5,3 +5,17 @@ import (
 
 	"github.com/mitchellh/mapstructure"
 )
+
+func GetLocations(options map[string]interface{}) (*AllLocations, error) {
+endpoint := endpointLocation
+
+hasParams := false
+params := make(map[string]string)
+
+if options == nil {
+   options = map[string]interface{}{
+      "endpoint": endpoint,
+   }
+}
+
+
