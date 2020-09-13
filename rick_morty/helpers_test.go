3fc78b37470b3d3b09266d5c9edb2181625b5164
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-14 02:46:47 +0530

    examples

---
+++ rick_morty/helpers_test.go
@@ -0,0 +1,16 @@
+package rickandmorty
+
+import "testing"
+
+var slice = []string{
+	"hello",
+	"world",
+	"hola",
+	"mundo",
+	"Allo",
+	"Welt",
+}
+
+var sliceIntegers = []int{
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+}
