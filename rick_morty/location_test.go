bb264338cfca174183c71043313f0f8ff3e2d430
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-15 00:44:15 +0530

    examples

--- rick_morty/location_test.go
+++ rick_morty/location_test.go
@@ -8,3 +8,18 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestGetLocationsFirstPage(t *testing.T) {
+options := map[string]interface{}{"page": 1}
+
+locations, err := GetLocations(options)
+if err != nil {
+   t.Error(err)
+}
+
+data, err := readFile("test-data/locations_first-page.json")
+if err != nil {
+   t.Error(err)
+}
+
+pagedResults := new(AllLocations)
