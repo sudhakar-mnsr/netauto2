7d05a5575d995c82046ff4ac8e07c4cbd76a3b0f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-18 23:43:35 +0530

    examples

--- cgroups.go
+++ cgroups.go
@@ -13,3 +13,20 @@ GetPids() ([]int, error)
 
 // Returns the PID's inside the cgroup set & all sub-cgroups
 GetAllPids() ([]int, error)
+
+// Returns statistics for the cgroup set
+GetStats() (*Stats, error)
+
+// Toggles the freezer cgroup according with specified state
+Freeze(state configs.FreezerState) error
+
+// Destroys the cgroup set
+Destroy() error
+
+// Path returns a cgroup path to the specified controller/subsystem
+// For cgroupv2, the argument is unused and can be empty.
+Path(string) string
+
+// Sets the cgroup as configured.
+Set(container *configs.Config) error
+
