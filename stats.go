ad8b9185802019488c650c59c9c2aff859d1814e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-19 00:46:15 +0530

    examples

--- stats.go
+++ stats.go
@@ -8,3 +8,26 @@ type ThrottlingData struct {
    // Aggregate time the container was throttled for in nanoseconds.
    ThrottledTime uint64 `json:"throttled_time,omitempty"`
 }
+
+// CPUUsage denotes the usage of the CPU
+// All CPU stats are aggregate since container inception
+type CpuUsage struct {
+   // Total CPU time consumed
+   // Units: nanoseconds
+   TotalUsage uint64 `json:"total_usage, omitempty"`
+   // Total CPU time consumed per core.
+   // Units: nanoseconds
+   PercpuUsage []uint64 `json:"percpu_usage,omitempty"`
+   // CPU time consumed per core in kernel mode
+   // Units: nanoseconds
+   PercpuUsageInKernelmode []unit64 `json:"percpu_usage_in_kernelmode"`
+   // CPU time consumed per core in user mode
+   // Units: nanoseconds.
+   PercpuUsageInUsermode []uint64 `json:"percpu_usage_in_usermode"`
+   // Time spent by tasks of the cgroup in user mode.
+   // Units: nanoseconds
+   UsageInKernelmode uint64 `json:"usage_in_kernelmode"`
+   // Time spent by tasks of the cgroup in user mode
+   // Units: nanoseconds
+   UsageInUsermode uint64 `json:"usage_in_usermode"`
+}
