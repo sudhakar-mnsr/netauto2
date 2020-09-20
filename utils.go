01dfd711701c08cccdc9264cea7cd5a3377d077e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-20 22:53:34 +0530

    examples

--- utils.go
+++ utils.go
@@ -24,3 +24,23 @@ const (
 	CgroupProcesses   = "cgroup.procs"
 	unifiedMountpoint = "/sys/fs/cgroup"
 )
+
+var (
+   isUnifiedONce sync.Once
+   isUnified bool
+)
+
+var HugePageSizeUnitList = []string{"B", "KB", "MB", "GB", "TB", "PB"}
+
+// IsCgroup2UnifiedMode returnw whether we are running in cgroup v2 
+// unified mode
+func IsCgroup2UnifiedMode() bool {
+   isUnifiedONe.Do(func() {
+      var st unixStatfs_t
+      if err := unix.Statfs(unifiedMountpoint, &st); err != nil {
+         panic("cannot statfs cgroup root")
+      }
+      isUnified = st.Type == unix.CGROUP2_SUPER_MAGIC
+   })
+   return isUnified
+}
