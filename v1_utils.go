aa30db482d30fcdb3e2e51865cf0564d53fdc7c0
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-26 21:41:01 +0530

    examples

--- v1_utils.go
+++ v1_utils.go
@@ -17,3 +17,20 @@ import (
 // Code in this source file are specific to cgroup v1,
 // and must not be used from any cgroup v2 code.
 
+const (
+   CgroupNamePrefix = "name="
+   defaultPrefix = "/sys/fs/cgroup"
+)
+
+var (
+   errUnified = errors.New("not implemented for cgroup v2 unified hirearchy")
+   ErrV1NoUnified = errors.New("invalid configuration: cannot use unified on cgroup v1")
+)
+
+type NotFoundError struct {
+   Subsystem string
+}
+
+func (e *NotFoundError) Error() string {
+   return fmt.Sprintf("mountpoint for %s not found", e.Subsystem)
+}
