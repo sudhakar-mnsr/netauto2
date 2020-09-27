93808973ba897c7389511822f8d408924e3b3476
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-28 02:26:20 +0530

    examples

--- REST/chitchat/route_auth.go
+++ REST/chitchat/route_auth.go
@@ -17,3 +17,22 @@ func login(writer http.ResponseWriter, request *http.Request) {
 func signup(writer http.ResponseWriter, request *http.Request) {
    generateHTML(writer, nil, "login.layout", "public.navbar", "signup")
 }
+
+// POST /signup
+// Create the user account
+func signupAccount(writer http.ResponseWriter, request *http.Request) {
+   err := request.ParseForm()
+   if err != nil {
+      danger(err, "Cannot parse form")
+   }
+   user := data.User{
+      Name: request.PostFormValue("name"),
+      Email: request.PostFormValue("email")
+      Password: request.PostFormValue("password"),
+   }
+   
+   if err := user.Create(); err != nil {
+      danger(err, "Cannot create user")
+   }
+   http.Redirect(writer, request, "/login", 302)
+}
