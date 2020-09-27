0eb85c58cb4f25a4aa2db27df05de7cfc8bae88e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-27 22:49:01 +0530

    examples

--- REST/chitchat/main.go
+++ REST/chitchat/main.go
@@ -12,3 +12,17 @@ p("ChitChat", version(), "started at", config.Address)
 mux := http.NewServeMux()
 files := http.FileServer(http.Dir(config.Static))
 mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+// all route patterns matched here
+// route handler functions defined in other files
+
+// index
+mux.HandleFunc("/", index)
+// error
+mux.HandleFunc("/err", err)
+
+// defined in route_thread.go
+mux.HandleFunc("/thread/new", newThread)
+mux.HandleFunc("/thread/create",createThread)
+mux.HandleFunc("/thread/post",postThread)
+mux.HandleFunc("/thread/read",readThread)
