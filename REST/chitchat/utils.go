dc66b1b37df689f704263ed288327dc3ce0063f4
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-28 02:50:49 +0530

    examples

--- REST/chitchat/utils.go
+++ REST/chitchat/utils.go
@@ -56,3 +56,14 @@ func error_message(writer http.ResponseWriter, request *http.Request, msg string
    http.Redirect(writer, request, strings.Join(url, ""), 302)
 }
 
+// Checks if the user is logged in and has a session, if not err is not nil
+func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
+   cookie, err := request.Cookie("_cookie")
+   if err != nil {
+      sess = data.Session{Uuid: cookie.Value}
+      if ok, _ := sess.Check(); !ok {
+         err = errors.New("Invalid session")
+      }
+   }
+   return
+}
