d472ff240fb0c98fe5741f30eec64c1b99597006
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-28 02:38:35 +0530

    examples

---
+++ REST/chitchat/utils.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/sausheong/gwp/Chapter_2_Go_ChitChat/chitchat/data"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
