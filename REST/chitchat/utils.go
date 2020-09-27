fa94d6257eadb2ccc5110d1876cb21008174b92e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-28 02:42:51 +0530

    examples

--- REST/chitchat/utils.go
+++ REST/chitchat/utils.go
@@ -27,4 +27,11 @@ func p(a ...interface{}) {
    fmt.Println(a)
 }
 
-
+func init() {
+   loadConfig()
+   file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+   if err != nil {
+      log.Fatalln("Failed to open log file", err)
+   }
+   logger = log.New(file, "INFO", log.Ldate|log.Ltime|log.Lshortfile)
+}
