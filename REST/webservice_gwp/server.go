3687049bf6415b96ec4ce408114b9537f7c9e49f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-29 13:03:31 +0530

    examples

--- REST/webservice_gwp/server.go
+++ REST/webservice_gwp/server.go
@@ -80,13 +80,23 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 // Update a post
 // PUT /post/1
 func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
-id, err := strconv.Atoi(path.Base(r.URL.Path))
-if err != nil {
+   id, err := strconv.Atoi(path.Base(r.URL.Path))
+   if err != nil {
+      return
+   }
+   post, err := retrieve(id)
+   if err != nil {
+      return
+   }
+   len := r.ContentLength
+   
+   body := make([]byte, len)
+   r.Body.Read(body)
+   json.Unmarshal(body, &post)
+   err = post.update()
+   if err != nil {
+      return
+   }
+   w.WriteHeader(200)
    return
 }
-post, err := retrieve(id)
-if err != nil {
-   return
-}
-len := r.ContentLength
-
