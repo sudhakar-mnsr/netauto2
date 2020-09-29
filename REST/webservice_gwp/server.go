f12a0640e5e9dd509323816fd7184352cf1dff2b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-29 13:05:45 +0530

    examples

--- REST/webservice_gwp/server.go
+++ REST/webservice_gwp/server.go
@@ -100,3 +100,22 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
    w.WriteHeader(200)
    return
 }
+
+// Delete a post
+// DELETE /post/1
+func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+   id, err := strconv.Atoi(path.Base(r.URL.Path))
+   if err != nil {
+      return
+   }
+   post, err := retrieve(id)
+   if err != nil {
+      return
+   }
+   err = post.delete()
+   if err != nil {
+      return
+   }
+   w.WriteHeader(200)
+   return
+}
