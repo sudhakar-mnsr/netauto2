257c3a6627130c010fb8935d69c7fba8f425cad4
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-29 12:40:24 +0530

    examples

--- REST/webservice_gwp/server.go
+++ REST/webservice_gwp/server.go
@@ -12,3 +12,11 @@ type Post struct {
    Content string `json:"content"`
    Author string `json:"author"`
 }
+
+func main() {
+   server := http.Server{
+      Addr: ":8080",
+   }
+   http.HandleFunc("/post/", handleRequest)
+   server.ListenAndServe()
+}
