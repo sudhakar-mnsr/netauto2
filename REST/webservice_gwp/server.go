15f4fa938ce0b00c0c0730166e7c9a146542ccd3
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-29 12:43:33 +0530

    examples

--- REST/webservice_gwp/server.go
+++ REST/webservice_gwp/server.go
@@ -20,3 +20,22 @@ func main() {
    http.HandleFunc("/post/", handleRequest)
    server.ListenAndServe()
 }
+
+// main handler function
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+   var err error
+   switch r.Method {
+   case "GET":
+      err = handleGet(w, r)
+   case "POST":
+      err = handlePost(w, r)
+   case "PUT":
+      err = handlePut(w, r)
+   case "DELETE": 
+      err = handleDelete(w, r)
+   }
+   if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+   }
+}
