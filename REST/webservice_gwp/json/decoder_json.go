a027ffb18f139f393bfb260cf38009585f33705f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-30 22:26:30 +0530

    examples

---
+++ REST/webservice_gwp/json/decoder_json.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
