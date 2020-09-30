d9937facaa50cebdc43846fe96adef8539aecf89
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-30 22:31:40 +0530

    examples

--- REST/webservice_gwp/json/decoder_json.go
+++ REST/webservice_gwp/json/decoder_json.go
@@ -26,14 +26,24 @@ type Comment struct {
 }
 
 func main() {
-jsonFile, err := os.Open("post.json")
-if err != nil {
-   fmt.Println("Error opening JSON file:", err)
-   return
+   jsonFile, err := os.Open("post.json")
+   if err != nil {
+      fmt.Println("Error opening JSON file:", err)
+      return
+   }
+   defer jsonFile.Close()
+   
+   decoder := json.NewDecoder(jsonFile)
+   for {
+      var post Post
+      err := decoder.Decode(&post)
+      if err == io.EOF {
+         break
+      }
+      if err != nil {
+         fmt.Println("Error decoding JSON:", err)
+         return
+      }
+      fmt.Println(post)
+   }
 }
-defer jsonFile.Close()
-
-decoder := json.NewDecoder(jsonFile)
-for {
-   var post Post
-
