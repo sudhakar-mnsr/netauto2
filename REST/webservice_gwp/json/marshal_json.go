ec918b9e42be3170277aec5fe0efc23fc03cd973
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-30 22:25:20 +0530

    examples

--- REST/webservice_gwp/json/marshal_json.go
+++ REST/webservice_gwp/json/marshal_json.go
@@ -25,23 +25,35 @@ type Comment struct {
 }
 
 func main {
-post := Post{
-   Id: 1,
-   Content: "Hello World!",
-   Author: Author{
-      Id: 2,
-      Name: "Sau Sheong",
-   },
-   Comments: []Comment{
-      Comment{
-         Id: 1,
-         Content: "Have a great day!",
-         Author: "Adam",
-      },
-      Comment{
+   post := Post{
+      Id: 1,
+      Content: "Hello World!",
+      Author: Author{
          Id: 2,
-         Content: "How are you today!",
-         Author: "Betty",
+         Name: "Sau Sheong",
       },
-   },
+      Comments: []Comment{
+         Comment{
+            Id: 1,
+            Content: "Have a great day!",
+            Author: "Adam",
+         },
+         Comment{
+            Id: 2,
+            Content: "How are you today!",
+            Author: "Betty",
+         },
+      },
+   }
+   
+   output, err := json.MarshalIndent(&post, "", "\t\t")
+   if err != nil {
+      fmt.Println("Error marshalling to JSON:", err)
+      return
+   }
+   err = ioutil.WriteFile("post.json", output, 0644)
+   if err != nil {
+      fmt.Println("Error writing JSON to file:", err)
+      return
+   }
 }
