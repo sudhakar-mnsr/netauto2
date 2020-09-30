48543a740f6afd22a37d723e779eabf7e4e56efd
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-09-30 22:12:17 +0530

    examples

--- REST/webservice_gwp/json/json.go
+++ REST/webservice_gwp/json/json.go
@@ -12,3 +12,36 @@ type Post struct {
    Author Author `json:"author"`
    Comments []Comment `json:"comments"`
 }
+
+type Author struct {
+   Id int `json:"id"`
+   Name string `json:"name"`
+}
+
+type Comment struct {
+   Id int `json:"id"`
+   Content string `json:"content"`
+   Author string `json:"author"`
+}
+
+func main() {
+post := Post{
+   Id: 1,
+   Content: "Hello World!",
+   Author: Author{
+      Id: 2,
+      Name: "Sau Sheong",
+   },
+   Comments: []Comment{
+      Comment{
+         Id: 1,
+         Content: "Have a great day!",
+         Author: "Adam",
+      },
+      Comment{
+         Id: 2,
+         Content: "How are you today?",
+         Author: "Betty",
+      },
+   },
+}
