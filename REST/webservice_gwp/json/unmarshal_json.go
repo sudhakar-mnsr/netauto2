2ad8db127417c9896fc89bf9c6aafccdedaabac2
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-01 23:55:05 +0530

    examples

--- REST/webservice_gwp/json/unmarshal_json.go
+++ REST/webservice_gwp/json/unmarshal_json.go
@@ -13,3 +13,14 @@ type Post struct {
 	Author   Author    `json:"author"`
 	Comments []Comment `json:"comments"`
 }
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
