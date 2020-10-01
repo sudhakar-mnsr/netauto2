21589ebb9f42c58f1a4f3d83360900eda76b4912
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-02 00:00:09 +0530

    examples

---
+++ REST/webservice_gwp/json/xml.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Post struct {
+	XMLName xml.Name `xml:"post"`
+	Id      string   `xml:"id,attr"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
+}
