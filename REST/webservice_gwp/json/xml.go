c93982813170c43db53ac1be84ab07101551e700
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-02 00:01:36 +0530

    examples

--- REST/webservice_gwp/json/xml.go
+++ REST/webservice_gwp/json/xml.go
@@ -12,3 +12,18 @@ type Post struct {
 	Content string   `xml:"content"`
 	Author  Author   `xml:"author"`
 }
+
+type Author struct {
+	Id   string `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+func main() {
+post := Post{
+   Id: "1",
+   Content: "Hello World!",
+   Author: Author{
+      Id: "2",
+      Name: "Sau Sheong",
+   },
+} 
