5ac021de4290770f00aea327e843c8c7fde4ebdf
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-03 14:38:02 +0530

    examples

--- REST/dbutils/rest_mongo.go
+++ REST/dbutils/rest_mongo.go
@@ -11,3 +11,18 @@ import (
         mgo "gopkg.in/mgo.v2"
         "gopkg.in/mgo.v2/bson"
 )
+
+// DB stores the database session information. Needs to be initialized once
+type DB sruct {
+   session *mgo.Session
+   collection *mgo.Collection
+}
+
+type Movie struct {
+   ID bson.ObjectId `json:"id" bson:"_id,omitempty"`
+   Name string `json:"name" bson:"name"`
+   Year string `json:"year" bson:"year"`
+   Directors []string `json:"directors" bson:"directors"`
+   Writers []string `json:"writers" bson:"writers"`
+   BoxOffice BoxOffice `json:"boxOffice" bson:"boxOffice"`
+}
