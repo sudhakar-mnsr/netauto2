6564fa87fbad9eadc9e1b4d3ef0faa60d116e872
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-03 07:53:34 +0530

    examples

--- REST/dbutils/main.go
+++ REST/dbutils/main.go
@@ -11,6 +11,30 @@ import (
    "github.com/sudhakar-mnsr/dbutils"
 )
 
+var DB *sql.DB
+// TrainResource is model for holding rail
+type TrainResource struct {
+   ID int
+   DriverName string
+   OperatingStatus bool
+}
+
+// StationResource holds information about locations
+type StationResource struct {
+   ID int
+   Name string
+   OpeningTime time.Time
+   ClosingTime time.Time
+}
+
+// ScheduleResource links both trains and stations
+type ScheduleResource struct {
+   ID int
+   TrainID int
+   StationID int
+   ArrivalTime time.Time
+}
+
 func main() {
    db, err :=sql.Open("sqlite3", "./railapi.db")
    if err != nil {
