838b0a431ecd4224cd63894c6a38fea195a37e53
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-03 07:44:43 +0530

    examples

---
+++ REST/dbutils/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+   "database/sql"
+   "log"
+   _ "github.com/mattn/go-sqlite3"
+   "github.com/sudhakar-mnsr/dbutils"
+)
+
+func main() {
+   db, err :=sql.Open("sqlite3", "./railapi.db")
+   if err != nil {
+      log.Println("Driver creation failed!")
+   }
+   dbutils.Initialize(db)
+}
