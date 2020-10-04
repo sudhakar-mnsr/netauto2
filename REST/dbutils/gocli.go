22de70f0b9f497b1a60265e275eb15e7b511aa53
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-04 12:05:28 +0530

    examples

--- REST/dbutils/gocli.go
+++ REST/dbutils/gocli.go
@@ -7,19 +7,29 @@ import (
 )
 
 func main() {
-// Create new app
-app := cli.NewApp()
-
-// add flags with three arguments
-app.Flags = []cli.Flag {
-cli.StringFlag{
-Name: "name",
-Value: "stranger",
-Usage: "your wonderful name",
-},
-cli.IntFlag{
-Name: "age",
-Value: 0,
-Usage: "your graceful age",
-},
+   // Create new app
+   app := cli.NewApp()
+   
+   // add flags with three arguments
+   app.Flags = []cli.Flag {
+      cli.StringFlag{
+         Name: "name",
+         Value: "stranger",
+         Usage: "your wonderful name",
+      },
+      cli.IntFlag{
+         Name: "age",
+         Value: 0,
+         Usage: "your graceful age",
+      },
+   }
+   
+   // This function parses and brings data in cli.Context struct
+   app.Action = func(c *cli.Context) error {
+   // c.String, c.Int looks for value of given flag
+   log.Printf("Hello %s (%d years), Welcome to the command line world", c.String("name", c.Int("age"))
+   return nil
+   }
+   // Pass os.Args to cli app to parse content
+   app.Run(os.Args)
 }
