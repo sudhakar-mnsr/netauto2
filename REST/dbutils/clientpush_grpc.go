0b4ba1a67d618ff6a2eb15fbfdfdf6e5e4d291bd
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-04 08:21:47 +0530

    examples

--- REST/dbutils/clientpush_grpc.go
+++ REST/dbutils/clientpush_grpc.go
@@ -15,8 +15,21 @@ const (
 
 // RecieveStream listens to the stream contents and use them
 func RecieveStream(client pb.MoneyTransactionClient, request *pb.TransactionRequest) {
-log.Println("Started listening to the server stream!")
-stream, err := client.MakeTransaction(context.Background(), request)
-if err != nil {
-   log.Fatalf("%v.MakeTransaction(_) = _, %v", client, err)
+   log.Println("Started listening to the server stream!")
+   stream, err := client.MakeTransaction(context.Background(), request)
+   if err != nil {
+      log.Fatalf("%v.MakeTransaction(_) = _, %v", client, err)
+   }
+   // Listen to the stream of messages
+   for {
+      response, err := stream.Recv()
+      if err == io.EOF {
+         // if there are not more messages, get out of loop
+      break
+      }
+      if err != nil {
+         log.Fatalf("%v.MakeTransaction(_) = _, %v", client, err)
+      }
+      log.Printf("Status: %v, Operation: %v", response.Status, response.Description)
+   }
 }
