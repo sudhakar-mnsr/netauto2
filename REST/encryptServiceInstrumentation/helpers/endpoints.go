a8787e4aa0a4ca1b54c8091aa9eeac86f99a0400
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-07 14:20:52 +0530

    examples

---
+++ REST/encryptServiceInstrumentation/helpers/endpoints.go
@@ -0,0 +1,12 @@
+package helpers
+
+import (
+"context"
+"github.com/go-kit/kit/endpoint"
+)
+
+// EncryptService is a blueprint for our service
+type EncryptService interface {
+   Encrypt(context.Context, string, string) (string, error)
+   Decrypt(context.Context, string, string) (string, error)
+}
