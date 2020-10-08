497533ed06356779d2de0c30a747cd1ebf3d8e8e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-09 02:48:32 +0530

    examples

--- REST/encryptServiceInstrumentation/helpers/middleware.go
+++ REST/encryptServiceInstrumentation/helpers/middleware.go
@@ -11,3 +11,20 @@ type LoggingMiddleware struct {
    Logger log.Logger
    Next EncryptService
 }
+
+// Encrypt logs the encryption requests
+func (mw LoggingMiddleware) Encrypt(ctx context.Context, key string, text string) (output string, err error) {
+   defer func(begin time.Time) {
+      _ = mw.Logger.Log(
+         "method", "encrypt",
+         "key", key,
+         "text", text,
+         "output", output,
+         "err", err,
+         "took", time.Since(begin),
+      )
+   }(time.Now())
+   
+   output, err = mw.Next.Encrypt(ctx, key, text)
+   return
+}
