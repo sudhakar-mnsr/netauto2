9bc34a949023584d9fd80b3e8a160b4db2ff12bd
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-09 03:06:14 +0530

    examples

--- REST/encryptServiceLogging/main.go
+++ REST/encryptServiceLogging/main.go
@@ -10,11 +10,19 @@ import (
 )
 
 func main() {
-logger := kitlog.NewLogfmtLogger(os.Stderr)
-var svc helpers.EncryptService
-svc = helpers.EncryptServiceInstance{}
-svc = helpers.LoggingMiddleware{Logger: logger, Next: svc}
-encryptHandler := httptransport.NewServer(helpers.MakeEncryptEndpoint(svc),
-   helpers.DecodeEncryptRequest, 
-   helpers.EncodeResponse)
-
+   logger := kitlog.NewLogfmtLogger(os.Stderr)
+   var svc helpers.EncryptService
+   svc = helpers.EncryptServiceInstance{}
+   svc = helpers.LoggingMiddleware{Logger: logger, Next: svc}
+   encryptHandler := httptransport.NewServer(helpers.MakeEncryptEndpoint(svc),
+      helpers.DecodeEncryptRequest, 
+      helpers.EncodeResponse)
+   
+   decryptHandler := httptransport.NewServer(helpers.MakeDecryptEndpoint(svc),
+      helpers.DecodeDecryptRequest,
+      helpers.EncodeResponse)
+   
+   http.Handle("/encrypt", encryptHandler)
+   http.Handle("/decrypt", decryptHandler)
+   log.Fatal(http.ListenAndServe(":8080", nil))
+}
