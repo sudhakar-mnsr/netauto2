9d50ab9e58354f83280c852adc9c951809e3275b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-08 11:33:01 +0530

    examples

--- REST/encryptServiceInstrumentation/helpers/jsonutils.go
+++ REST/encryptServiceInstrumentation/helpers/jsonutils.go
@@ -5,3 +5,12 @@ import (
 	"encoding/json"
 	"net/http"
 )
+
+// DecodeEncryptRequest fills struct from JSON details of request
+func DecodeEncryptRequest(_ context.Context, r *http.Request) (interface{}, error) {
+   var request EncryptRequest
+   if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+      return nil, err
+   }
+   return request, nil
+}
