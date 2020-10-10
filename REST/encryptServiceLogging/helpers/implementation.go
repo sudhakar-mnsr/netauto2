3abccce51b3e2909099613fdd23e1954a366559b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-10 23:29:47 +0530

    examples

--- REST/encryptServiceLogging/helpers/implementation.go
+++ REST/encryptServiceLogging/helpers/implementation.go
@@ -31,10 +31,19 @@ func (EncryptServiceInstance) Encrypt(_ context.Context, key string, text string
 
 // Decrypt decrypts the encrypted string to original
 func (EncryptServiceInstance) Decrypt(_ context.Context, key string, text string) (string, error) {
-if key == "" || text == "" {
-   return "", errEmpty
+   if key == "" || text == "" {
+      return "", errEmpty
+   }
+   block, err := aes.NewCipher([]byte(key))
+   if err != nil {
+      panic(err)
+   }
+   
+   ciphertext, _ := base64.StdEncoding.DecodeString(text)
+   cfb := cipher.NewCFBEncrypter(block, initVector)
+   plaintext := make([]byte, len(ciphertext))
+   cfb.XORKeyStream(plaintext, ciphertext))
+   return string(plaintext), nil
 }
-block, err := aes.NewCipher([]byte(key))
-if err != nil {
-   panic(err)
-}
+
+var errEmpty = errors.New("Sectt Key or Text should not be empty")
