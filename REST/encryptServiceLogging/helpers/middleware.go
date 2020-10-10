562b058a068fbdca02b801c6486be159defd43a2
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-11 00:45:35 +0530

    examples

--- REST/encryptServiceLogging/helpers/middleware.go
+++ REST/encryptServiceLogging/helpers/middleware.go
@@ -29,3 +29,19 @@ func (mw LoggingMiddleware) Encrypt(ctx context.Context, key string, text sring)
    return
 }
 
+// Decrypt logs the decryption requests
+func (mw LoggingMiddleware) Decrypt(ctx context.Context, key string, text string) (output string, err error) {
+   defer func(begin time.Time) {
+      _ = mw.Logger.Log(
+         "method", "decrypt",
+         "key", key,
+         "message", text,
+         "output", output,
+         "err", err,
+         "took", time.Since(begin),
+      )
+   }(time.Now())
+   
+   output, err = mw.Next.Decrypt(ctx, key, text)
+   return
+}
