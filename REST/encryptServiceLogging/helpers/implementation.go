78474046237ca27edadd3d27f0b5c87b31f9fac0
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-10 23:24:59 +0530

    examples

--- REST/encryptServiceLogging/helpers/implementation.go
+++ REST/encryptServiceLogging/helpers/implementation.go
@@ -18,7 +18,13 @@ var initVector = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 
 
 // Encrypt encrypts the string with given key
 func (EncryptServiceInstance) Encrypt(_ context.Context, key string, text string) (string, error) {
-block, err := aes.NewCipher([]byte(key))
-if err != nil {
-   panic(err)
+   block, err := aes.NewCipher([]byte(key))
+   if err != nil {
+      panic(err)
+   }
+   plaintext := []byte(text)
+   cfb := cipher.NewCFBEncrypter(block, initVector)
+   ciphertext := make([]byte, len(plaintext))
+   cfb.XORKeyStream(ciphertext, plaintext)
+   return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
