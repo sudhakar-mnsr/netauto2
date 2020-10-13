1823006af36a37220e595a7fe332d9f88408075e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-13 12:39:49 +0530

    examples

--- REST/encryptServiceLogging/mirrorFinder/main.go
+++ REST/encryptServiceLogging/mirrorFinder/main.go
@@ -37,9 +37,20 @@ func findFastest(urls []string) response {
 }
 
 func main() {
-http.HandleFunc("/fastest-mirror", func(w http.ResponseWriter, r *http.Request) {
-response := findFastest(mirrors.MirrorList)
-respJSON, _ := json.Marshal(response)
-w.Header().Set("Content-Type", "application/json")
-w.Write(respJSON)
-})
+   http.HandleFunc("/fastest-mirror", func(w http.ResponseWriter, r *http.Request) {
+      response := findFastest(mirrors.MirrorList)
+      respJSON, _ := json.Marshal(response)
+      w.Header().Set("Content-Type", "application/json")
+      w.Write(respJSON)
+   })
+   
+   port := ":8000"
+   server := &http.Server{
+      Addr: port,
+      ReadTimeout: 10 * time.Second,
+      WriteTimeout: 10 *time.Second,
+      MaxHeaderBytes: 1 << 20,
+   }
+   fmt.Printf("Starting server on port %s\n", port)
+   log.Fatal(server.ListenAndServe())
+}
