7573bacd0867e777a352f3f94d43ee4ff853abbc
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-13 17:55:00 +0530

    added new service

--- REST/new/main.go
+++ REST/new/main.go
@@ -16,3 +16,15 @@ type Book struct {
    Author string
    PublishedYear string
 }
+
+func main() {
+// File open for reading, writing and appending
+f, err := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+if err != nil {
+   fmt.Printf("error opening file: %v", err)
+}
+defer f.Close()
+
+// This attaches program logs to file
+log.SetOutput(f)
+
