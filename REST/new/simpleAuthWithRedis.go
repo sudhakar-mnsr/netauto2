7be1ebace7fc962795ba7644c1dafbb91afa0ae7
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-14 23:38:35 +0530

    examples

--- REST/new/simpleAuthWithRedis.go
+++ REST/new/simpleAuthWithRedis.go
@@ -15,3 +15,10 @@ var users = map[string]string{"naren": "passme", "admin": "password"}
 
 // HealthcheckHandler returns the date and time
 func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+   session, _ := store.Get(r, "session.id")
+   if (session.Values["authenticated"] != nil) && session.Values["authenticated"] != false {
+      w.Write([]byte(time.Now().String()))
+   } else {
+      http.Error(w, "Forbidden", http.StatusForbidden)
+   }
+}
