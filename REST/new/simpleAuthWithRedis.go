c9c495521b5b7b8f32ba9de3f9ddffe39e8f99fe
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-14 23:36:04 +0530

    examples

--- REST/new/simpleAuthWithRedis.go
+++ REST/new/simpleAuthWithRedis.go
@@ -9,3 +9,9 @@ import (
 	"github.com/gorilla/mux"
 	redistore "gopkg.in/boj/redistore.v1"
 )
+
+var strore, err = redistore.NewRediStore(10, "tcp", ":6379", "", []byte(os.Getenv("SESSION_SECRET")))
+var users = map[string]string{"naren": "passme", "admin": "password"}
+
+// HealthcheckHandler returns the date and time
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
