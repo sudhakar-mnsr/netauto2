fe715155a252f4672122970b6e5e0a84f954fadc
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-15 23:39:06 +0530

    examples

--- REST/railapi/main.go
+++ REST/railapi/main.go
@@ -38,3 +38,16 @@ func (t *TrainResource) Register(container *restful.Container) {
    ws.Route(ws.DELETE("/{train-id}").To(t.removeTrain))
    container.Add(ws)
 }
+
+// GET http://localhost:8000/v1/trains/1
+func (t TrainResource) getTrain(request *restful.Request, response *restful.Response) {
+   id := request.PathParameter("train-id")
+   err := DB.QueryRow("select ID, DRIVER_NAME, OPERATING_STATUS FROM train where id=?", id), id).Scan(&t.ID, &t.DriverName, &t.OperatingStatus)
+   if err != nil {
+      log.Println(err)
+      response.AddHeader("Content-Type", "text/plain")
+      response.WriteErrorString(http.StatusNotFound, "Train could not be found.")
+   } else {
+      response.WriteEntity(t)
+   }
+}
