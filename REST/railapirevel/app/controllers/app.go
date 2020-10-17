6d7c6d9a865ddea1de2ba8a751223d595c8ef349
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-17 23:29:23 +0530

    examples

--- REST/railapirevel/app/controllers/app.go
+++ REST/railapirevel/app/controllers/app.go
@@ -17,3 +17,10 @@ type TrainResource struct {
    DriverName string `json:"driver_name"`
    OperatingStatus bool `json:"operating_status"`
 }
+
+// GetTrain handles GET on train resource
+var train TrainResource
+// Getting the values from path parameters.
+id := c.Params.Route.Get("train-id")
+// use this ID to query from database and fill train table..
+train.ID, _ = strconv.Atoi(id)
