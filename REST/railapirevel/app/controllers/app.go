e11b0464fc22757072bfe8a666d4f623afb26cc9
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-17 23:31:07 +0530

    examples

--- REST/railapirevel/app/controllers/app.go
+++ REST/railapirevel/app/controllers/app.go
@@ -19,8 +19,14 @@ type TrainResource struct {
 }
 
 // GetTrain handles GET on train resource
-var train TrainResource
-// Getting the values from path parameters.
-id := c.Params.Route.Get("train-id")
-// use this ID to query from database and fill train table..
-train.ID, _ = strconv.Atoi(id)
+func (c App) GetTrain() revel.Result {
+   var train TrainResource
+   // Getting the values from path parameters.
+   id := c.Params.Route.Get("train-id")
+   // use this ID to query from database and fill train table..
+   train.ID, _ = strconv.Atoi(id)
+   train.DriverName = "Logan"
+   train.OperatingStatus = true
+   c.Response.Status = http.StatusOK
+   return c.RenderJSON(train)
+}
