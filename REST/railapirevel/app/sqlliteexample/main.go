7d9eb0e199264cdfcba529f928d11a048232821b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-18 00:07:19 +0530

    examples

--- REST/railapirevel/app/sqlliteexample/main.go
+++ REST/railapirevel/app/sqlliteexample/main.go
@@ -13,3 +13,12 @@ name string
 author string
 }
 
+func dbOperations(db *sql.DB) {
+// Create
+statement, _ := db.Prepare("INSERT INTO books (name, author, isbn) VALUES (?, ?, ?)")
+statement.Exec("A Tale of two cities", "Charless Dickens", 140430547)
+log.Println("Inserted the book into database!")
+
+// Read
+rows, _ := db.Query("SELECT id, name, author FROM books")
+var tempBook Book
