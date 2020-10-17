2d9dd345ce677b59abbc7a7e6a25fe095e0df5f7
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-17 23:27:47 +0530

    examples

--- REST/railapirevel/app/controllers/app.go
+++ REST/railapirevel/app/controllers/app.go
@@ -11,3 +11,9 @@ type App struct {
    *revel.Controller
 }
 
+// TrainResource is the model for holding rail information
+type TrainResource struct {
+   ID int `json:"id"`
+   DriverName string `json:"driver_name"`
+   OperatingStatus bool `json:"operating_status"`
+}
