0521482579520a10329d483b0ea0b3bd94bd4889
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-17 23:23:54 +0530

    examples

--- REST/railapirevel/app/init.go
+++ REST/railapirevel/app/init.go
@@ -10,18 +10,32 @@ var (
 }
 
 func init() {
-// Filters is the default set of global filters.
-revel.Filters = []revel.Filter{
-   revel.PanicFilter,
-   revel.RouterFilter,
-   revel.FilterConfiguringFilter,
-   revel.ParamsFilter,
-   revel.SessionFilter,
-   revel.FlashFilter,
-   revel.ValidationFilter,
-   revel.I18nFilter,
-   HeaderFilter,
-   revel.InterceptorFilter,
-   revel.CompressFilter,
-   revel.ActionInvoker,
+   // Filters is the default set of global filters.
+   revel.Filters = []revel.Filter{
+      revel.PanicFilter,
+      revel.RouterFilter,
+      revel.FilterConfiguringFilter,
+      revel.ParamsFilter,
+      revel.SessionFilter,
+      revel.FlashFilter,
+      revel.ValidationFilter,
+      revel.I18nFilter,
+      HeaderFilter,
+      revel.InterceptorFilter,
+      revel.CompressFilter,
+      revel.ActionInvoker,
+   }
 }
+
+// HeaderFilter adds common security headers
+// TODO turn this into revel.HeaderFilter
+// should probably also have a filter for CSRF
+// not sure if it can go in the same filter or not
+var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
+c.Response.Out.Header().Add("X-Frame-Options", "SAMEORIGIN")
+c.Response.Out.Header().Add("X-XSS-Protection", "1; mode=block")
+c.Response.Out.Header().Add("X-Content-Type-Options, "nosniff")
+
+fc[0](c, fc[1:])
+}
+
