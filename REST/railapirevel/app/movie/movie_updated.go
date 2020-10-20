639b0ef3860411938f5b524a4f9accd4ac9ecb7e
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-20 22:59:37 +0530

    examples

--- REST/railapirevel/app/movie/movie_updated.go
+++ REST/railapirevel/app/movie/movie_updated.go
@@ -21,3 +21,17 @@ import (
 type DB struct {
 	collection *mongo.Collection
 }
+
+type Movie struct {
+   ID interface{} `json:"id" bson:"_id, omitempty"`
+   Name string `json:"name" bson:"name"`
+   Year string `json:"year" bson:"year"`
+   Directors []string `json:"directors" bson:"directors"`
+   Writers []string `json:"writers" bson:"writers"`
+   BoxOffice BoxOffice `json:"boxOffice" bson:"boxOffice"`
+}
+
+type BoxOffice struct {
+   Budget uint64 `json:"budget" bson:"budget"`
+   Gross uint64 `json:"gross" bson:"gross"`
+}
