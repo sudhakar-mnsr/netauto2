94880f80d7e9520ccea2ff8e6e559a2a44fa3b5d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-21 23:57:47 +0530

    examples

--- REST/railapirevel/app/movie/vedor.go
+++ REST/railapirevel/app/movie/vedor.go
@@ -24,3 +24,18 @@ type BoxOffice struct {
 	Budget uint64 `bson:"budget"`
 	Gross  uint64 `bson:"gross"`
 }
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB successfully")
+
