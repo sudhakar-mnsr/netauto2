664b9584a6429fcc70393c539a9a01ae969eebbc
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-21 23:56:21 +0530

    examples

--- REST/railapirevel/app/movie/vedor.go
+++ REST/railapirevel/app/movie/vedor.go
@@ -10,3 +10,17 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// Movie holds a movie data
+type Movie struct {
+	Name      string   `bson:"name"`
+	Year      string   `bson:"year"`
+	Directors []string `bson:"directors"`
+	Writers   []string `bson:"writers"`
+	BoxOffice `bson:"boxOffice"`
+}
+
+// BoxOffice is nested in Movie
+type BoxOffice struct {
+	Budget uint64 `bson:"budget"`
+	Gross  uint64 `bson:"gross"`
+}
