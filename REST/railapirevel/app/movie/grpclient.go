0485af1d2a74369c483568f9e3518e27f9c7ca0a
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-22 23:43:30 +0530

    examples

--- REST/railapirevel/app/movie/grpclient.go
+++ REST/railapirevel/app/movie/grpclient.go
@@ -11,3 +11,16 @@ import (
 const (
 	address = "localhost:50051"
 )
+
+func main() {
+// setup a connection to the server
+conn, err := grpc.Dial(address, grpc.WithInsecure())
+if err != nil {
+   log.Fatal("Did not connect: %v", err)
+}
+
+defer conn.Close()
+c := pb.NewMoneyTransactionClient(conn)
+
+// Prepare data. Get this from clients like frontend or App
+form := "1234"
