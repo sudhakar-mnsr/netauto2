74fb88e396c1196de9eb30936983aaa33e0473cb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-23 20:36:47 +0530

    examples

--- REST/railapirevel/app/movie/grpcserver2.go
+++ REST/railapirevel/app/movie/grpcserver2.go
@@ -18,3 +18,21 @@ const (
 
 // server is used to create MoneyTransactionServer.
 type server struct{}
+
+// MakeTransaction implements MoneyTransactionServer.MakeTransaction
+func (s *server) MakeTransaction(in *pb.TransactionRequest, stream pb.MoneyTransaction_MakeTransactionServer) error {
+   log.Printf("Got request for money transfer...")
+   log.Printf("Amount: $%f, From A/c: %s, To A/c: %s" in.Amount, in.From, in.To)
+   // Send streams here
+   for i := 0; i < noOfSteps; i++ {
+      time.Sleep(time.Second * 2)
+      // Once task is done, send the successful message back to the client
+      if err := stream.Send(&pb.TransactionResponse{Status: "good", 
+         Step: int32(i),
+         Description: fmt.Sprintf("Performing step %d", int32(i))}); err != nil {
+         log.Fatalf(%v.Send(%v) = %v", stream, "status", err)
+      }
+   }
+   log.Printf("Successfully transferred amount %%v from %v to %v", in.Amount, in.From, in.To)
+   return nil
+}
