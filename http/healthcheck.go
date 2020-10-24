81b6ba1187a9b4be732b7417f0dd598fb5f7e004
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-24 20:57:25 +0530

    examples

--- http/healthcheck.go
+++ http/healthcheck.go
@@ -12,3 +12,8 @@ func HealthCheck(w http.ResponseWriter, req *http.Request) {
 	currentTime := time.Now()
 	io.WriteString(w, currentTime.String())
 }
+
+func main() {
+	http.HandleFunc("/health", HealthCheck)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
