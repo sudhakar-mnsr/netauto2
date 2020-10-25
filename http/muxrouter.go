5485d1706948cf8e66f3c321509f24ef44439a97
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-25 23:43:33 +0530

    examples

--- http/muxrouter.go
+++ http/muxrouter.go
@@ -8,3 +8,10 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
+	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
+}
