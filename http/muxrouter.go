849ef31fc71d7248238bf7afdf876d31400eb542
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-25 23:43:55 +0530

    examples

--- http/muxrouter.go
+++ http/muxrouter.go
@@ -15,3 +15,15 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
 	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
 }
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
