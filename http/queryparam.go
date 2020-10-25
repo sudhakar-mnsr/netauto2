dea5e29e1a5c3b37c3fc64396c83c21e476d15e5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-25 23:44:55 +0530

    examples

--- http/queryparam.go
+++ http/queryparam.go
@@ -8,3 +8,11 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+// QueryHandler handles the given query parameters
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Got parameter id:%s!\n", queryParams["id"][0])
+	fmt.Fprintf(w, "Got parameter category:%s!", queryParams["category"][0])
+}
