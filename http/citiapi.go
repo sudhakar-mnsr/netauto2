d7ef5f1723db97cc823056f8993ad77301c91f28
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-26 23:17:40 +0530

    examples

--- http/citiapi.go
+++ http/citiapi.go
@@ -19,3 +19,12 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
+		defer r.Body.Close()
+		fmt.Printf("Got %s city with area of %d sq miles!\n", tempCity.Name, tempCity.Area)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("201 - Created"))
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("405 - Method Not Allowed"))
+	}
+}
