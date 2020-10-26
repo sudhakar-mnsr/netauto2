6f80078ac8c7ff6da8ce79fd768f7401f862fa7f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-26 23:13:43 +0530

    examples

--- http/uuid.go
+++ http/uuid.go
@@ -9,3 +9,12 @@ import (
 // UUID is a custom multiplexer
 type UUID struct {
 }
+
+func (p *UUID) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		giveRandomUUID(w, r)
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
