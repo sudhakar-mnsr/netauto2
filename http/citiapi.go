ca994f2a59e9af7c23ab6ff3f55ee7368a30dee4
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-26 23:17:21 +0530

    examples

--- http/citiapi.go
+++ http/citiapi.go
@@ -10,3 +10,12 @@ type city struct {
 	Name string
 	Area uint64
 }
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var tempCity city
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&tempCity)
+		if err != nil {
+			panic(err)
+		}
