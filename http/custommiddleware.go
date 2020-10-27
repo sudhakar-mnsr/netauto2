2a10930275c0537927e2f8d0b839bbfc850946b8
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-27 21:53:00 +0530

    examples

--- http/custommiddleware.go
+++ http/custommiddleware.go
@@ -4,3 +4,12 @@ import (
 	"fmt"
 	"net/http"
 )
+
+func middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Executing middleware before request phase!")
+		// Pass control back to the handler
+		handler.ServeHTTP(w, r)
+		fmt.Println("Executing middleware after response phase!")
+	})
+}
