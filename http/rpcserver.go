a14e6d0663f3988be859c816de130965bf09ffcb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-28 21:02:59 +0530

    examples

--- http/rpcserver.go
+++ http/rpcserver.go
@@ -13,3 +13,17 @@ import (
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
 )
+
+// Args holds arguments passed to JSON RPC service
+type Args struct {
+	ID string
+}
+
+// Book struct holds Book JSON structure
+type Book struct {
+	ID     string `json:"id,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Author string `json:"author,omitempty"`
+}
+
+type JSONServer struct{}
