ae15fc7715cdb54d57cd9bbf01eba73e649c3c84
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-28 21:06:10 +0530

    examples

--- http/rpcserver.go
+++ http/rpcserver.go
@@ -60,3 +60,9 @@ func main() {
 	s := rpc.NewServer()
 	// Register the type of data requested as JSON
 	s.RegisterCodec(json.NewCodec(), "application/json")
+	// Register the service by creating a new JSON server
+	s.RegisterService(new(JSONServer), "")
+	r := mux.NewRouter()
+	r.Handle("/rpc", s)
+	http.ListenAndServe(":1234", r)
+}
