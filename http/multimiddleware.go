3220a7510c5edaa8d0d97cceb20d46245f524c6d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-29 22:57:36 +0530

    examples

--- http/multimiddleware.go
+++ http/multimiddleware.go
@@ -17,3 +17,11 @@ func filterContentType(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Currently in the check content type middleware")
 		// Filtering requests by MIME type
+		if r.Header.Get("Content-type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type. Please send JSON"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
