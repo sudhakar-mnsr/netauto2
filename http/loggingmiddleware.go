0d074e3473f8c59a72ed420b2f18c6d0b9831ff5
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-29 23:00:13 +0530

    examples

--- http/loggingmiddleware.go
+++ http/loggingmiddleware.go
@@ -14,3 +14,10 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 	log.Println("Finished processing request")
 }
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handle)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":8000", loggedRouter)
+}
