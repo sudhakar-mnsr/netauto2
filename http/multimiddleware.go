cef6075fcc4f0c9f26ec4f4c7ddc78e673150444
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-29 22:57:19 +0530

    examples

--- http/multimiddleware.go
+++ http/multimiddleware.go
@@ -12,3 +12,8 @@ type city struct {
 	Name string
 	Area uint64
 }
+
+func filterContentType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Currently in the check content type middleware")
+		// Filtering requests by MIME type
