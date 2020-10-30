a0693956767c277ad34d33f669666646396eb4c1
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-30 23:02:33 +0530

    examples

--- http/rpcserver1.go
+++ http/rpcserver1.go
@@ -11,3 +11,10 @@ import (
 type Args struct{}
 
 type TimeServer int64
+
+func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
+	// Fill reply pointer to send the data back
+	*reply = time.Now().Unix()
+	return nil
+}
+
