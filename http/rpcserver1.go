321ccc2095e44c2330f55dc716f369bc28c5211d
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-30 23:02:58 +0530

    examples

--- http/rpcserver1.go
+++ http/rpcserver1.go
@@ -18,3 +18,14 @@ func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
 	return nil
 }
 
+func main() {
+	timeserver := new(TimeServer)
+	rpc.Register(timeserver)
+	rpc.HandleHTTP()
+	// Listen for requests on port 1234
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	http.Serve(l, nil)
+}
