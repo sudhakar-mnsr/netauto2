1985ddf2fa78e92fcf6fca145d1e07e1dc0f2b77
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-31 11:09:11 +0530

    examples

--- http/jwt_auth.go
+++ http/jwt_auth.go
@@ -91,3 +91,18 @@ func getTokenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/getToken", getTokenHandler)
+	r.HandleFunc("/healthcheck", HealthcheckHandler)
+	http.Handle("/", r)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
