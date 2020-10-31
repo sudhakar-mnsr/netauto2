3ca226c2acd607e6e309381fc325ebcf3f2ecb32
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-31 11:05:05 +0530

    examples

---
+++ http/jwt_auth.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"github.com/gorilla/mux"
+)
