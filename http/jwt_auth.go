42b77480c70c7880d8bd22f512ed7adf7bae1ddb
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-10-31 11:06:06 +0530

    examples

--- http/jwt_auth.go
+++ http/jwt_auth.go
@@ -30,3 +30,12 @@ func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return secretKey, nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Access Denied; Please check the access token"))
+		return
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
