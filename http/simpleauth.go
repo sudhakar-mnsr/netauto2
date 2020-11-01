304b522540958eb0458d0d77afc6589e9d26c3ff
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-01 22:50:30 +0530

    examples

--- http/simpleauth.go
+++ http/simpleauth.go
@@ -12,3 +12,13 @@ import (
 
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
 var users = map[string]string{"naren": "passme", "admin": "password"}
+
+// HealthcheckHandler returns the date and time
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session.id")
+	if (session.Values["authenticated"] != nil) && session.Values["authenticated"] != false {
+		w.Write([]byte(time.Now().String()))
+	} else {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+	}
+}
