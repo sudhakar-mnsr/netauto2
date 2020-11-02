0b5d99e9dc08fbd0a26a1b6c4dd59fa3584de4f1
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-02 17:07:59 +0530

    examples

--- http/simple_auth_redis.go
+++ http/simple_auth_redis.go
@@ -9,3 +9,16 @@ import (
 	"github.com/gorilla/mux"
 	redistore "gopkg.in/boj/redistore.v1"
 )
+
+var store, err = redistore.NewRediStore(10, "tcp", ":6379", "", []byte(os.Getenv("SESSION_SECRET")))
+var users = map[string]string{"naren": "passme", "admin": "password"}
+
+// HealthcheckHandler returns the date and time
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session.id")
+	if (session.Values["authenticated"] != nil) && session.Values["authenticated"] != false {
+		w.Write([]byte(time.Now().String()))
+	} else {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+	}
+}
