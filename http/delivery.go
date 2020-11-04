59e61f408d1a65bf669849db4f87295b8e33d075
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-04 22:16:10 +0530

    examples

--- http/delivery.go
+++ http/delivery.go
@@ -9,3 +9,18 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
 )
+
+// Movie holds a movie data
+type Movie struct {
+	Name      string   `bson:"name"`
+	Year      string   `bson:"year"`
+	Directors []string `bson:"directors"`
+	Writers   []string `bson:"writers"`
+	BoxOffice `bson:"boxOffice"`
+}
+
+// BoxOffice is nested in Movie
+type BoxOffice struct {
+	Budget uint64 `bson:"budget"`
+	Gross  uint64 `bson:"gross"`
+}
