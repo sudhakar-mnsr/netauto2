7493fa82b4b350c414fcd17af2aa00d870346c4b
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-06 23:09:17 +0530

    examples

--- http/movie_updated.go
+++ http/movie_updated.go
@@ -105,3 +105,11 @@ func (db *DB) DeleteMovie(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Deleted succesfully!"))
 	}
 }
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(context.TODO())
