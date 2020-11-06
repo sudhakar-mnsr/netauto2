412ab45b018811ee3ac83d873e87ea4bfb9e6466
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-06 23:09:56 +0530

    examples

--- http/movie_updated.go
+++ http/movie_updated.go
@@ -121,3 +121,11 @@ func main() {
 	r.HandleFunc("/v1/movies", db.PostMovie).Methods("POST")
 	r.HandleFunc("/v1/movies/{id:[a-zA-Z0-9]*}", db.UpdateMovie).Methods("PUT")
 	r.HandleFunc("/v1/movies/{id:[a-zA-Z0-9]*}", db.DeleteMovie).Methods("DELETE")
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
