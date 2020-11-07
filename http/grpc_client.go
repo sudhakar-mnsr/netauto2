095ed74f8f690f676c9b1fa88c9d077cf9de8d12
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-07 12:41:58 +0530

    examples

--- http/grpc_client.go
+++ http/grpc_client.go
@@ -25,3 +25,12 @@ func main() {
 	from := "1234"
 	to := "5678"
 	amount := float32(1250.75)
+
+	// Contact the server and print out its response.
+	r, err := c.MakeTransaction(context.Background(), &pb.TransactionRequest{From: from,
+		To: to, Amount: amount})
+	if err != nil {
+		log.Fatalf("Could not transact: %v", err)
+	}
+	log.Printf("Transaction confirmed: %t", r.Confirmation)
+}
