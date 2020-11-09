b38ee219ac5d9c923dafe6e36beaa013f439d07f
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-11-09 13:20:04 +0530

    examples

--- http/url_shortner.go
+++ http/url_shortner.go
@@ -24,3 +24,12 @@ type Record struct {
 	ID  int    `json:"id"`
 	URL string `json:"url"`
 }
+
+// GetOriginalURL fetches the original URL for the given encoded(short) string
+func (driver *DBClient) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
+	var url string
+	vars := mux.Vars(r)
+	// Get ID from base62 string
+	id := base62.ToBase10(vars["encoded_string"])
+	err := driver.db.QueryRow("SELECT url FROM web_url WHERE id = $1", id).Scan(&url)
+	// Handle response details
